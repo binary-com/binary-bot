b944f5faf6c13dffba901bf4a5da2031503825a5
Amin <marashi.s.amin@gmail.com>
2016-04-30 17:45:13 +0430

    Version 1.1.4: fallback to english, merged win/loss and up/down modules, added zoom to blockly, Completed translations and separated xml loading from bot.html

--- src/globals/version.js
+++ src/globals/version.js
@@ -1,5 +1,5 @@
 Bot.Version = function Version(){
-	Bot.version = '1.1.3';
+	Bot.version = '1.1.4';
 	if (Bot.debug) {
 		console.log('%cBinary Bot (v' + Bot.version + ') started.', 'color: green');
 	} else {
