9afcc969ac44c4a1f6677a76fcd2746d787db66e
Amin <marashi.s.amin@gmail.com>
2016-05-23 12:18:10 +0430

    Added 404 to the gulp deploy script

--- gulpfile.js
+++ gulpfile.js
@@ -217,7 +217,7 @@ gulp.task('build-min', ['build-bot-min', 'build-index-min', 'pack-css-min', 'mus
 });
 
 gulp.task('deploy', ['build-min'], function () {
-	return gulp.src(['LICENSE', 'README.md', 'CNAME', './www/**'])
+	return gulp.src(['404.md', 'LICENSE', 'README.md', 'CNAME', './www/**'])
 		.pipe(ghPages());
 });
 
