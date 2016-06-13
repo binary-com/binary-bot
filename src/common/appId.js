c492215d36e649dbacc319ed81e4c06a97d80955
Amin Marashi <marashi.s.amin@gmail.com>
2016-06-13 12:37:45 +0800

    Added appid of my github.io for development

--- src/common/appId.js
+++ src/common/appId.js
@@ -1,7 +1,7 @@
 var utils = require('utils');
 
 var AppId = {
-	app_id: ( document.location.port === '8080' ) ? 1168 : 1169,
+	app_id: ( document.location.port === '8080' ) ? 1168 : ( document.location.hostname.indexOf('github.io') >= 0 ) ? 1180 : 1169,
 	redirectOauth: function oauthLogin(){
 		document.location = 'https://oauth.binary.com/oauth2/authorize?app_id=' + this.app_id + '&l=' + window.lang.toUpperCase();
 	},
