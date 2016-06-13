6c563e783e5f9a046ff1b486784e4e3767316fd8
Amin Marashi <marashi.s.amin@gmail.com>
2016-06-13 15:23:26 +0800

    Version 1.2.0: All markets added

--- src/bot/globals/version.js
+++ src/bot/globals/version.js
@@ -1,5 +1,5 @@
 var globals = require('./globals');
-var version = '1.1.9';
+var version = '1.2.0';
 if (globals.debug) {
 	console.log('%cBinary Bot (v' + version + ') started.', 'color: green');
 } else {
