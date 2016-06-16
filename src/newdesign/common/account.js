12bc2ea4e3489dd72238a28f2d181814567a9228
Amin Marashi <marashi.s.amin@gmail.com>
2016-06-16 09:56:59 +0800

    Fixed the account test problem with ws

--- src/newdesign/common/account.js
+++ src/newdesign/common/account.js
@@ -1,10 +1,13 @@
 var LiveApi = require('binary-live-api').LiveApi;
 var storageManager = require('./storageManager');
-var ws = require('./mockWs');
-
 module.exports = {
 	addTokenIfValid: function addTokenIfValid(token, callback) {
-		var api = new LiveApi();
+		var api;
+		if (typeof ws === 'undefined') {
+			api = new LiveApi({ websocket: require('ws') });
+		} else {
+			api = new LiveApi();
+		}
 		api.authorize(token)
 			.then(function (response) {
 				api.disconnect();
