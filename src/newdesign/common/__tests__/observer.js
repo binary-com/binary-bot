dbda958da9b32ad536436e4f99d280217606fd1a
Amin Marashi <marashi.s.amin@gmail.com>
2016-06-16 16:33:28 +0800

    Added test for the observer

---
+++ src/newdesign/common/__tests__/observer.js
@@ -0,0 +1,25 @@
+var observer = require('../observer');
+var expect = require('chai').expect;
+require('./browser');
+
+describe('Observer', function(){
+	var returnedValue = null;
+	before(function(done){
+		observer.register('global.createVar', function(obj){
+			window[obj.name] = obj.text;
+			return obj.name;
+		});
+		setTimeout(function(){
+			returnedValue = observer.trigger('global.createVar', {
+				name: 'newVar',
+				text: 'Hello, thanks for defining me'
+			});
+			done();
+		}, 1000);
+	});
+	it('observer should trigger the action defined for an event', function(){
+		expect(window.newVar).to.be.a('string')
+			.and.to.be.equal('Hello, thanks for defining me');
+		expect(returnedValue).to.be.equal('newVar');
+	});
+});
