997d5fb5155aa526fffdc203a9e72bc97a16fb63
Amin Marashi <marashi.s.amin@gmail.com>
2016-06-23 14:14:25 +0800

    Update 2016-06-23T06:12:33.053Z

--- src/bot/code_generators/finish/check_result.js
+++
@@ -1,6 +0,0 @@
-var blockly = require('blockly');
-blockly.JavaScript.contract_check_result = function(block) {
-	var check_with = block.getFieldValue('CHECK_RESULT');
-	var code = '(result === \'' + check_with + '\')';
-  return [code, blockly.JavaScript.ORDER_ATOMIC];
-};
