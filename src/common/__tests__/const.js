afca67e2a6afe71d1670b0347698dfb061f0459f
Amin Marashi <marashi.s.amin@gmail.com>
2016-08-04 14:41:15 +0800

    Removed version from const test

--- src/common/__tests__/const.js
+++ src/common/__tests__/const.js
@@ -3,7 +3,7 @@ import _const from '../const';
 import {expect} from 'chai';
 
 describe('Const', function(){
-	it('Const should contain lists, opposites, oppositesHaveBarrier, conditionsCategory, conditionsCategoryName, conditions, version', function(){
+	it('Const should contain lists, opposites, oppositesHaveBarrier, conditionsCategory, conditionsCategoryName, conditions', function(){
 		expect(_const).to.have.all.keys([
 			'lists',
 			'opposites',
