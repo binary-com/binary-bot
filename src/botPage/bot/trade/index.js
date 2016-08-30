d574faaf7bbc1f4ad72cbf6736daf6b09c649d0d
Amin Marashi <marashi.s.amin@gmail.com>
2016-08-30 14:53:46 +0800

    Refactored trade to es6

--- src/botPage/bot/trade/index.js
+++ src/botPage/bot/trade/index.js
@@ -1,100 +1,79 @@
-'use strict';
 import Observer from 'binary-common-utils/observer';
-import Translator from 'translator';
+import { translator } from '../../../common/translator';
 
-var Trade = function Trade(api) {
-	this.observer = new Observer();
-	this.api = api;
-	this.contractIsSold = false;
-	this.translator = new Translator();
-	this.runningObservations = [];
-};
-
-Trade.prototype = Object.create(null, {
-	recoverFromDisconnect: {
-		value: function recoverFromDisconnect(){
-			for ( var i in this.runningObservations  ) {
-				this.observer.unregisterAll.apply(this.observer, this.runningObservations[i]);
-			}
-			this.runningObservations = [];
-			return this.subscribeToOpenContract();
-		}
-	},
-	purchase: {
-		value: function purchase(contract) {
-			var that = this;
-			this.api.buy(contract.id, contract.ask_price);
-			var apiBuy = function apiBuy(purchasedContract){
-				that.observer.emit('ui.log.info', that.translator.translateText('Purchased') + ': ' + contract.longcode);
-				that.observer.emit('trade.purchase', purchasedContract);
-				that.contractId = purchasedContract.contract_id;
-				that.api._originalApi.unsubscribeFromAllProposals().then(function(){}, function reject(error){
-					that.observer.emit('api.error', error);
-				});
-				that.subscribeToOpenContract();
-			};
-			this.observer.register('api.buy', apiBuy, true, {
-				type: 'buy',
-				unregister: [['api.buy', apiBuy], 'trade.purchase']
-			});
-			this.runningObservations.push(['api.buy', apiBuy]);
-		}
-	},
-	subscribeToOpenContract: {
-		value: function subscribeToOpenContract(){
-			if ( !this.contractId ) {
-				return false;
-			}
-			this.api.proposal_open_contract(this.contractId);
-			var that = this;
-			var apiProposalOpenContract = function apiProposalOpenContract(contract){
-				// detect changes and decide what to do when proposal is updated
-				if (contract.is_expired && contract.is_valid_to_sell &&!that.contractIsSold ) {
-					that.contractIsSold = true;
-					that.api._originalApi.sellExpiredContracts().then(function(){
-						that.getTheContractInfoAfterSell();
-					}, function reject(error){
-						that.observer.emit('api.error', error);
-					});
-				}
-				if ( contract.sell_price ) {
-					that.observer.emit('trade.finish', contract);
-				}
-				that.observer.emit('trade.update', contract);
-			};
-			this.observer.register('api.proposal_open_contract', apiProposalOpenContract, false, {
-				type: 'proposal_open_contract',
-				unregister: [
-					['api.proposal_open_contract', apiProposalOpenContract],
-					'trade.update',
-					'strategy.tradeUpdate',
-					'trade.finish',
-					'strategy.finish'
-				]
-			});
-			this.runningObservations.push(['api.proposal_open_contract', apiProposalOpenContract]);
-			return true;
-		}
-	},
-	getTheContractInfoAfterSell: {
-		value: function getTheContractInfoAfterSell() {
-			if ( this.contractId ) {
-				this.api._originalApi.getContractInfo(this.contractId);
-			}
-		}
-	},
-	destroy: {
-		value: function destroy(offline){
-			for ( var i in this.runningObservations  ) {
-				this.observer.unregisterAll.apply(this.observer, this.runningObservations[i]);
-			}
-			this.runningObservations = [];
-			this.contractIsSold = false;
-			if ( !offline ) {
-				return this.api._originalApi.unsubscribeFromAlProposals();
-			}
-		}
-	}
-});
-
-module.exports = Trade;
+export default class Trade {
+  constructor(api) {
+    this.observer = new Observer();
+    this.api = api;
+    this.contractIsSold = false;
+    this.runningObservations = [];
+  }
+  recoverFromDisconnect() {
+    for (let obs of this.runningObservations) {
+      this.observer.unregisterAll(...obs);
+    }
+    this.runningObservations = [];
+    return this.subscribeToOpenContract();
+  }
+  purchase(contract) {
+    this.api.buy(contract.id, contract.ask_price);
+    let apiBuy = (purchasedContract) => {
+      this.observer.emit('ui.log.info', translator.translateText('Purchased') + ': ' + contract.longcode);
+      this.observer.emit('trade.purchase', purchasedContract);
+      this.contractId = purchasedContract.contract_id;
+      this.api._originalApi.unsubscribeFromAllProposals().then(() => 0, (e) => this.observer.emit('api.error', e));
+      this.subscribeToOpenContract();
+    };
+    this.observer.register('api.buy', apiBuy, true, {
+      type: 'buy',
+      unregister: [['api.buy', apiBuy], 'trade.purchase'],
+    });
+    this.runningObservations.push(['api.buy', apiBuy]);
+  }
+  subscribeToOpenContract() {
+    if (!this.contractId) {
+      return false;
+    }
+    this.api.proposal_open_contract(this.contractId);
+    let apiProposalOpenContract = (contract) => {
+      // detect changes and decide what to do when proposal is updated
+      if (contract.is_expired && contract.is_valid_to_sell && !this.contractIsSold) {
+        this.contractIsSold = true;
+        this.api._originalApi.sellExpiredContracts().then(() => this.getTheContractInfoAfterSell(),
+          (error) => this.observer.emit('api.error', error));
+      }
+      if (contract.sell_price) {
+        this.observer.emit('trade.finish', contract);
+      }
+      this.observer.emit('trade.update', contract);
+    };
+    this.observer.register('api.proposal_open_contract', apiProposalOpenContract, false, {
+      type: 'proposal_open_contract',
+      unregister: [
+        ['api.proposal_open_contract', apiProposalOpenContract],
+        'trade.update',
+        'strategy.tradeUpdate',
+        'trade.finish',
+        'strategy.finish',
+      ],
+    });
+    this.runningObservations.push(['api.proposal_open_contract', apiProposalOpenContract]);
+    return true;
+  }
+  getTheContractInfoAfterSell() {
+    if (this.contractId) {
+      this.api._originalApi.getContractInfo(this.contractId);
+    }
+  }
+  destroy(offline) {
+    for (let obs of this.runningObservations) {
+      this.observer.unregisterAll(...obs);
+    }
+    this.runningObservations = [];
+    this.contractIsSold = false;
+    if (!offline) {
+      return this.api._originalApi.unsubscribeFromAlProposals();
+    }
+		return null;
+  }
+}
