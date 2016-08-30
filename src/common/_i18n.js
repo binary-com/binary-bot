093fd791d01b0c6c152fd2c31ca26c0d7be3f41a
Amin Marashi <marashi.s.amin@gmail.com>
2016-08-30 14:04:06 +0800

    Moved i18n to es6

--- src/common/_i18n.js
+++ src/common/_i18n.js
@@ -1,43 +1,46 @@
-'use strict';
 import sha1 from 'sha1';
 
-module.exports = {
-	translation: null,
-	init: function init(options, callback) {
-		this.translation = options.resources[options.lng][options.defaultNS];
-	},
-	t: function t(key) {
-		return this.translation[key];
-	},
-	_: function _(str, opt) {
-		var key = sha1(str);
-		var result = this.t(key);
-		return (result === '') ? str : result;
-	},
-	xml: function xml(dom) {
-		var categories = dom.getElementsByTagName('category');
-		for ( var i = 0 ; i < categories.length ; i++ ) {
-			var child = categories[i];
-			var str = child.getAttribute('i18n-text');
-			var key;
-			var hasTranslation = false;
-			if (str === null) {
-				key = child.getAttribute('i18n');
-				if (key !== null) {
-					hasTranslation = true;
-				}
-			} else {
-				key = sha1(str);
-				hasTranslation = true;
-			}
-			var result = this.t(key);
-			if (hasTranslation) {
-				child.setAttribute('name', (result === '') ? str : result);
-			}
-			if (child.childNodes.length > 0) {
-				this.xml(child);
-			}
-		}
-		return dom;
-	}
-};
+class I18n {
+  constructor() {
+    this.translation = null;
+  }
+  init(options) {
+    this.translation = options.resources[options.lng][options.defaultNS];
+  }
+  t(key) {
+    return this.translation[key];
+  }
+  _(str) {
+    let key = sha1(str);
+    let result = this.t(key);
+    return (result === '') ? str : result;
+  }
+  xml(dom) {
+    let categories = dom.getElementsByTagName('category');
+    for (let i = 0; i < categories.length; i++) {
+      let child = categories[i];
+      let str = child.getAttribute('i18n-text');
+      let key;
+      let hasTranslation = false;
+      if (str === null) {
+        key = child.getAttribute('i18n');
+        if (key !== null) {
+          hasTranslation = true;
+        }
+      } else {
+        key = sha1(str);
+        hasTranslation = true;
+      }
+      let result = this.t(key);
+      if (hasTranslation) {
+        child.setAttribute('name', (result === '') ? str : result);
+      }
+      if (child.childNodes.length > 0) {
+        this.xml(child);
+      }
+    }
+    return dom;
+  }
+}
+
+export default new I18n();
