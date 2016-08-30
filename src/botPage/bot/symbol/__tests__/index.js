5a3c8757812ea6702472858b4fdfdbd69cdd0ed6
Amin Marashi <marashi.s.amin@gmail.com>
2016-08-30 15:23:45 +0800

    refactored symbol test

--- src/botPage/bot/symbol/__tests__/index.js
+++ src/botPage/bot/symbol/__tests__/index.js
@@ -1,77 +1,89 @@
-'use strict';
-import _Symbol from '../index';
-import tools from 'binary-common-utils/tools';
-import {expect} from 'chai';
-import ws from 'mock/websocket';
+import { expect } from 'chai';
 import CustomApi from 'binary-common-utils/customApi';
+import ws from '../../../../common/mock/websocket';
+import _Symbol from '../index';
 
-describe('symbol', function() {
-	describe('Error Handling', function(){
-		it('initializing is needed for symbol functions', function(){
-			expect(function(){_Symbol.getAllowedConditions();}).to.throw(Error);
-			expect(function(){_Symbol.isConditionAllowedInSymbol();}).to.throw(Error);
-			expect(function(){_Symbol.getConditionName();}).to.throw(Error);
-			expect(function(){_Symbol.getCategoryForCondition();}).to.throw(Error);
-			expect(function(){_Symbol.getCategoryNameForCondition();}).to.throw(Error);
-			expect(function(){_Symbol.getAllowedCategoryNames();}).to.throw(Error);
-			expect(function(){_Symbol.findSymbol();}).to.throw(Error);
-		});
-	});
-	describe('Checking functions', function(){
-		
-		var symbol;
-		before(function(done){
-			symbol = new _Symbol(new CustomApi(ws));
-			symbol.initPromise.then(function(){
-				done();
-			});
-		});
-		it('findSymbol returns symbol if exist', function(){
-			expect(symbol.findSymbol('R_100')).to.be.ok
-				.and.to.have.property('R_100');
-			expect(symbol.findSymbol('FAKE')).not.to.be.ok;
-		});
-		it('getAllowedCategoryNames returns allowed category names', function(){
-			expect(symbol.getAllowedCategoryNames('R_100')).to.be.ok
-				.and.to.have.all.members([ 'Up/Down', 'Digits', 'Asians', 'Touch/No Touch', 'Ends In/Out', 'Stays In/Goes Out' ]);
-			expect(symbol.getAllowedCategoryNames('FAKE')).to.be.empty;
-		});
-		it('getCategoryNameForCondition returns category name of a condition', function(){
-			expect(symbol.getCategoryNameForCondition('risefall'))
-				.to.be.equal('Up/Down');
-		});
-		it('getCategoryForCondition returns category of a condition', function(){
-			expect(symbol.getCategoryForCondition('risefall'))
-				.to.be.equal('callput');
-		});
-		it('getConditionName returns name of a condition', function(){
-			expect(symbol.getConditionName('risefall'))
-				.to.be.equal('Rise/Fall');
-		});
-		it('isConditionAllowedInSymbol returns true if a condition is allowed in a symbol', function(){
-			expect(symbol.isConditionAllowedInSymbol('R_100', 'risefall'))
-				.to.be.ok;
-			expect(symbol.isConditionAllowedInSymbol('frxEURUSD', 'asians'))
-				.not.to.be.ok;
-			expect(symbol.isConditionAllowedInSymbol('fake', 'asians'))
-				.not.to.be.ok;
-			expect(symbol.isConditionAllowedInSymbol('frxEURUSD', 'fake'))
-				.not.to.be.ok;
-		});
-		it('getAllowedConditionsForSymbol returns allowed conditions for a symbol', function(){
-			expect(symbol.getAllowedConditionsForSymbol('R_100'))
-				.to.have.all.members([ 'risefall', 'higherlower', 'matchesdiffers', 'evenodd', 'overunder', 'asians', 'touchnotouch', 'endsinout', 'staysinout' ]);
-			expect(symbol.getAllowedConditionsForSymbol('fake'))
-				.to.be.empty;
-		});
-		it('getAllowedCategoriesForSymbol returns allowed categories for a symbol', function(){
-			expect(symbol.getAllowedCategoriesForSymbol('R_100'))
-				.to.have.all.members([ 'callput', 'digits', 'asian', 'touchnotouch', 'endsinout', 'staysinout' ]);
-			expect(symbol.getAllowedCategoriesForSymbol('fake'))
-				.to.be.empty;
-		});
-	});
-	after(function(){
-		delete _Symbol.instance;
-	});
+describe('symbol', () => {
+  describe('Error Handling', () => {
+    it('initializing is needed for symbol functions', () => {
+      expect(() => {
+        _Symbol.getAllowedConditions();
+      }).to.throw(Error);
+      expect(() => {
+        _Symbol.isConditionAllowedInSymbol();
+      }).to.throw(Error);
+      expect(() => {
+        _Symbol.getConditionName();
+      }).to.throw(Error);
+      expect(() => {
+        _Symbol.getCategoryForCondition();
+      }).to.throw(Error);
+      expect(() => {
+        _Symbol.getCategoryNameForCondition();
+      }).to.throw(Error);
+      expect(() => {
+        _Symbol.getAllowedCategoryNames();
+      }).to.throw(Error);
+      expect(() => {
+        _Symbol.findSymbol();
+      }).to.throw(Error);
+    });
+  });
+  describe('Checking functions', () => {
+    let symbol;
+    before(function(done) {
+      symbol = new _Symbol(new CustomApi(ws));
+      symbol.initPromise.then(() => {
+        done();
+      });
+    });
+    it('findSymbol returns symbol if exist', () => {
+      expect(symbol.findSymbol('R_100')).to.be.ok
+        .and.to.have.property('R_100');
+      expect(symbol.findSymbol('FAKE')).not.to.be.ok;
+    });
+    it('getAllowedCategoryNames returns allowed category names', () => {
+      expect(symbol.getAllowedCategoryNames('R_100')).to.be.ok
+        .and.to.have.all.members(['Up/Down', 'Digits', 'Asians', 'Touch/No Touch', 'Ends In/Out', 'Stays In/Goes Out']);
+      expect(symbol.getAllowedCategoryNames('FAKE')).to.be.empty;
+    });
+    it('getCategoryNameForCondition returns category name of a condition', () => {
+      expect(symbol.getCategoryNameForCondition('risefall'))
+        .to.be.equal('Up/Down');
+    });
+    it('getCategoryForCondition returns category of a condition', () => {
+      expect(symbol.getCategoryForCondition('risefall'))
+        .to.be.equal('callput');
+    });
+    it('getConditionName returns name of a condition', () => {
+      expect(symbol.getConditionName('risefall'))
+        .to.be.equal('Rise/Fall');
+    });
+    it('isConditionAllowedInSymbol returns true if a condition is allowed in a symbol', () => {
+      expect(symbol.isConditionAllowedInSymbol('R_100', 'risefall'))
+        .to.be.ok;
+      expect(symbol.isConditionAllowedInSymbol('frxEURUSD', 'asians'))
+        .not.to.be.ok;
+      expect(symbol.isConditionAllowedInSymbol('fake', 'asians'))
+        .not.to.be.ok;
+      expect(symbol.isConditionAllowedInSymbol('frxEURUSD', 'fake'))
+        .not.to.be.ok;
+    });
+    it('getAllowedConditionsForSymbol returns allowed conditions for a symbol', () => {
+      expect(symbol.getAllowedConditionsForSymbol('R_100'))
+				.to.have.all.members(['risefall', 'higherlower', 'matchesdiffers',
+					'evenodd', 'overunder', 'asians', 'touchnotouch', 'endsinout', 'staysinout']);
+      expect(symbol.getAllowedConditionsForSymbol('fake'))
+        .to.be.empty;
+    });
+    it('getAllowedCategoriesForSymbol returns allowed categories for a symbol', () => {
+      expect(symbol.getAllowedCategoriesForSymbol('R_100'))
+        .to.have.all.members(['callput', 'digits', 'asian', 'touchnotouch', 'endsinout', 'staysinout']);
+      expect(symbol.getAllowedCategoriesForSymbol('fake'))
+        .to.be.empty;
+    });
+  });
+  after(() => {
+    delete _Symbol.instance;
+  });
 });
