6ab5a58ba4a1423f2ee9f5d3f3f1e45be0370aa1
Amin Marashi <marashi.s.amin@gmail.com>
2016-08-30 13:58:03 +0800

    Updated common tests and const

--- src/common/__tests__/const.js
+++ src/common/__tests__/const.js
@@ -1,22 +1,20 @@
-'use strict';
+/* eslint-disable import/no-extraneous-dependencies */
+import { expect } from 'chai';
 import _const from '../const';
-import {expect} from 'chai';
 
-describe('Const', function(){
-	it('Const should contain lists, opposites, hasSecondBarrierOffset, hasBarrierOffset, hasPrediction, conditionsCategory, conditionsCategoryName, conditions', function(){
-		expect(_const).to.have.all.keys([
-			'lists',
-			'opposites',
-			'tickFields',
-			'ohlcFields',
-			'hasSecondBarrierOffset',
-			'hasBarrierOffset',
-			'hasPrediction',
-			'conditionsCategory',
-			'conditionsCategoryName',
-			'conditions',
-			'barrierTypes',
-			'durationTypes'
-		]);
-	});
+describe('Const', () => {
+  it('Const should contain all const defs', () => {
+    expect(_const).to.have.all.keys([
+      'lists',
+      'opposites',
+      'hasSecondBarrierOffset',
+      'hasBarrierOffset',
+      'hasPrediction',
+      'conditionsCategory',
+      'conditionsCategoryName',
+      'conditions',
+      'barrierTypes',
+      'durationTypes',
+    ]);
+  });
 });
--- src/common/__tests__/translator.js
+++ src/common/__tests__/translator.js
@@ -1,32 +1,32 @@
-'use strict';
-import Translator from '../translator';
-import tools from 'binary-common-utils/tools';
-import {expect} from 'chai';
+/* eslint-disable import/no-extraneous-dependencies */
+import { strToXml } from 'binary-common-utils/tools';
+import { expect } from 'chai';
+import { translator } from '../translator';
 
-describe('Translation', function(){
-	describe('Translate Functions', function(){
-		var toolbox;
-		var translator;
-		before(function(){
-			toolbox = tools.strToXml(`
+describe('Translation', () => {
+  describe('Translate Functions', () => {
+    let toolbox;
+    before(() => {
+      toolbox = strToXml(`
 				<category name="fake1" colour="15" i18n-text="Conditions">
 					<category name="fake2" colour="15" i18n-text="Up/Down">
 						<block type="risefall"></block>
 					</category>
 				</category>
 					`, 'text/xml');
-			translator = new Translator();
-		});
-		it('translateText', function(){
-			expect(translator.translateText('Logic')).to.be.a('string')
-				.and.to.be.equal('Logic');
-		});
-		it('translateXml', function(){
-			expect(translator.translateXml(toolbox)).to.have.property('firstChild')
-				.that.satisfy(function(dom){ return dom.getAttribute('name') === 'Conditions'; })
-				.and.to.have.property('childNodes')
-				.that.has.property('1')
-				.that.satisfy(function(dom){ return dom.getAttribute('name') === 'Up/Down'; });
-		});
-	});
+    });
+    it('translateText', () => {
+      expect(translator.translateText('Logic')).to.be.a('string')
+        .and.to.be.equal('Logic');
+    });
+    it('translateXml', () => {
+      expect(translator.translateXml(toolbox)).to.have.property('firstChild')
+        .that.satisfy((dom) => dom.getAttribute('name') === 'Conditions'
+      )
+        .and.to.have.property('childNodes')
+        .that.has.property('1')
+        .that.satisfy((dom) => dom.getAttribute('name') === 'Up/Down'
+      );
+    });
+  });
 });
