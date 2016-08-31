52e39dde00f81d1c31e0723b8fdcef0e7c96e4f2
Amin Marashi <marashi.s.amin@gmail.com>
2016-08-31 12:16:46 +0800

    refactored tools defs

--- src/botPage/view/blockly/definitions/tools/total_profit.js
+++ src/botPage/view/blockly/definitions/tools/total_profit.js
@@ -1,16 +1,13 @@
-'use strict';
 // https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#3bwqd4
-
 import { translator } from '../../../../../common/translator';
-
 
 Blockly.Blocks.total_profit = {
   init: function() {
     this.appendDummyInput()
-        .appendField(translator.translateText("Total Profit"));
-    this.setOutput(true, "Number");
-    this.setColour("#dedede");
+        .appendField(translator.translateText('Total Profit'));
+    this.setOutput(true, 'Number');
+    this.setColour('#dedede');
     this.setTooltip(translator.translateText('Returns the total profit'));
     this.setHelpUrl('https://github.com/binary-com/binary-bot/wiki');
-  }
+  },
 };
