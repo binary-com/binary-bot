b4252c884c6a7f5f953ef329cd49aaf1a913da7d
Amin Marashi <marashi.s.amin@gmail.com>
2016-08-31 13:27:39 +0800

    Refactoed tools code

--- src/botPage/view/blockly/code_generators/tools/total_runs.js
+++ src/botPage/view/blockly/code_generators/tools/total_runs.js
@@ -1,6 +1,4 @@
-'use strict';
-
 Blockly.JavaScript.total_runs = function(block) {
-	var code = 'Bot.getTotalRuns()';
+	let code = 'Bot.getTotalRuns()';
   return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
