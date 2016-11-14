0d147f250c1f451017ef6cafc545e3f279beaaac
Amin Marashi <marashi.s.amin@gmail.com>
2016-11-14 09:54:59 +0800

    Fixed the tradeTypes for adding all fields for bc

--- src/botPage/view/blockly/utils.js
+++ src/botPage/view/blockly/utils.js
@@ -15,7 +15,7 @@ export const updateInputList = (block) => {
 }
 
 export const setInputList = (block) => {
-  Blockly.Blocks.endsinout.init.call(block) // Assuming the endsinout trade type includes all possible inputs
+  Blockly.Blocks.allFields.init.call(block)
 }
 
 export const isMainBlock = (blockType) => config.mainBlocks.indexOf(blockType) >= 0
