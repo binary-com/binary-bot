454aa79c92f7a6d7eadb1c63a9ed9c02d038c77c
Amin Marashi <marashi.s.amin@gmail.com>
2016-11-22 14:19:27 +0800

    Added translation app id

--- src/common/appId.js
+++ src/common/appId.js
@@ -10,6 +10,8 @@ export function setAppId() {
     appId = 1180; // binary bot github.io
   } else if (document.location.pathname.indexOf('/beta') >= 0) {
     appId = 1261; // binary bot beta
+  } else if (document.location.pathname.indexOf('/translation') >= 0) {
+    appId = 1412; // binary bot translation
   } else {
     appId = 1169; // binary bot
   }
