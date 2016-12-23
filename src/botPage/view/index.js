b78c337faa9f69357cc0bef8993d279048d35e01
Amin Marashi <marashi.s.amin@gmail.com>
2016-12-23 10:25:10 +0800

    Fix reality check buttons

--- src/botPage/view/index.js
+++ src/botPage/view/index.js
@@ -24,6 +24,7 @@ const showRealityCheck = () => {
 }
 
 const hideRealityCheck = () => {
+  $('#rc-err').hide()
   $('.blocker').hide()
   $('.reality-check').hide()
 }
@@ -312,17 +313,17 @@ export default class View {
           .click()
       })
 
-    $('#logout')
+    $('#logout, #logout-reality-check')
       .click(() => {
         logout()
+        hideRealityCheck()
         $('.logout').hide()
       })
 
-    $('#continueTrading')
+    $('#continue-trading')
       .click(() => {
         const time = parseInt($('#realityDuration').val(), 10)
         if (time >= 10 && time <= 120) {
-          $('#rc-err').hide()
           hideRealityCheck()
           startRealityCheck(time)
         } else {
@@ -386,7 +387,7 @@ export default class View {
       })
       .text('Log in')
 
-    $('#accountHistory').click(() => {
+    $('#statement-reality-check').click(() => {
       document.location =
         `https://www.binary.com/${translator.getLanguage()}/user/statementws.html`
     })
