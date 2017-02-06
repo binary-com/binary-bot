fe8ffa299f5c7f923c2a39b0f28d6b8922676179
Amin Marashi <marashi.s.amin@gmail.com>
2017-02-06 11:55:17 +0800

    Refactor logger

--- src/botPage/common/logger.js
+++ src/botPage/common/logger.js
@@ -2,133 +2,104 @@ import { getToken } from 'binary-common-utils/lib/storageManager'
 import { translate } from '../../common/i18n'
 import { observer } from './shared'
 
+const log = (type, ...args) => {
+  if (type === 'warn') {
+    console.warn(...args) // eslint-disable-line no-console
+  } else if (type === 'error') {
+    console.error(...args) // eslint-disable-line no-console
+  } else {
+    console.log(...args) // eslint-disable-line no-console
+  }
+}
+
 const shown = []
 
-const isNew = (message) => {
+const isNew = msg => {
   const timestamp = parseInt(new Date().getTime() / 1000, 10)
-  const index = shown.findIndex((e) => e.message === message)
-  if (index >= 0) {
-    const oldTimestamp = shown[index].timestamp
-    shown[index].timestamp = timestamp
-    if (timestamp - oldTimestamp >= 1) {
-      return true
-    }
-    return false
+
+  const shownMsg = shown.find(e => e.msg === msg)
+  if (shownMsg) {
+    const oldTimestamp = shownMsg.timestamp
+
+    shownMsg.timestamp = timestamp
+    return (timestamp - oldTimestamp) >= 1
   }
-  shown.push({
-    message,
-    timestamp,
-  })
+
+  shown.push({ msg, timestamp })
   return true
 }
 
-const notify = (message, ...args) => {
-  if (isNew(message)) {
-    $.notify(message, ...args)
-  }
+const notifyUniq = (msg, ...args) => isNew(msg) && $.notify(msg, ...args)
+
+export const notify = (msg, className, position = 'left', ...rest) => {
+  log(className, msg, ...rest)
+  notifyUniq(msg, { position: `bottom ${position}`, className })
 }
 
 export const notifyError = (error) => {
   let message = (error.error) ?
-    error.error.message :
-    error.message || error
+    error.error.message : error.message || error
   const errorCode = error.error ?
-    error.error.code :
-    error.name
+    error.error.code : error.name
 
   if (error.name === 'DisconnectError') {
     message = translate('Connection lost before receiving the response from the server')
   }
 
-  const completeMsg = errorCode ?
-    `${errorCode}: ${message}` : message
-  notify(message, {
-    position: 'bottom right',
-    className: 'error',
-  })
-  console.warn(error) // eslint-disable-line no-console
-  console.error(completeMsg) // eslint-disable-line no-console
-  return message
+  const completeMsg = errorCode ? `${errorCode}: ${message}` : message
+
+  notify(message, 'error', 'right', error, completeMsg)
 }
 
+const errorList = ['api.error', 'BlocklyError', 'LimitsReached']
+
+const notifList = ['success', 'info', 'warn', 'error']
+
+const logList = [
+  'log.bot.start',
+  'log.bot.login',
+  'log.bot.proposal',
+  'log.purchase.start',
+  'log.trade.purchase',
+  'log.trade.update',
+  'log.trade.finish',
+]
+
+const amplitudeList = ['log.bot.login', 'log.trade.finish']
+
+logList.forEach(event =>
+  observer.register(event, d => log('info', event, d)))
+
+errorList.forEach(type => observer.register(type, e => notifyError(e)))
+
+notifList.forEach(className =>
+  observer.register(`ui.log.${className}`, message =>
+    notify(message, className, 'right')))
+
 export const logHandler = () => {
-  // catch known errors and log them
-  ['api.error', 'BlocklyError', 'RuntimeError', 'LimitsReached'].forEach(errorType =>
-    observer.register(errorType, (error) => { // eslint-disable-line no-loop-func
-      const message = notifyError(error)
-      amplitude.getInstance().logEvent(errorType, {
-        message,
-      })
-    }))
-
   const token = $('.account-id').first().attr('value')
-  const accountName = getToken(token).account_name
-  if (typeof amplitude !== 'undefined') {
-    amplitude.getInstance().setUserId(accountName)
-  }
-  if (typeof trackJs !== 'undefined') {
-    trackJs.configure({
-      userId: accountName,
-    })
-  }
+  const userId = getToken(token).account_name
 
-  const observeForLog = (type, position) => {
-    const subtype = (position === 'left') ? '.left' : ''
-    observer.register(`ui.log.${type}${subtype}`, (message) => {
-      if (type === 'warn') {
-        console.warn(message) // eslint-disable-line no-console
-      } else {
-        console.log(message) // eslint-disable-line no-console
-      }
-      if (position === 'left') {
-        $.notify(message, {
-          position: `bottom ${position}`,
-          className: type,
-        })
-      } else {
-        notify(message, {
-          position: `bottom ${position}`,
-          className: type,
-        })
-      }
-    })
-  }
+  if (amplitude) {
+    amplitude.getInstance().setUserId(userId)
 
-  ['success', 'info', 'warn', 'error'].forEach(type => {
-    observeForLog(type, 'right')
-    observeForLog(type, 'left')
-  })
+    amplitudeList.forEach(event =>
+      observer.register(event, (d) => amplitude.getInstance().logEvent(event, d)))
 
-  const logList = [
-    'log.bot.start',
-    'log.bot.login',
-    'log.bot.proposal',
-    'log.purchase.start',
-    'log.trade.purchase',
-    'log.trade.update',
-    'log.trade.finish',
-  ]
+    observer.register('log.revenue', (data) => {
+      const { user, profit, contract } = data
 
-  logList.forEach(event =>
-    observer.register(event, d => console.log(event, d))) // eslint-disable-line no-console
-
-  const amplitudeList = ['log.bot.login', 'log.trade.finish']
-
-  amplitudeList.forEach(event =>
-    observer.register(event, d => amplitude.getInstance().logEvent(event, d)))
-
-  observer.register('log.revenue', data => {
-    const { user, profit, contract } = data
-    if (typeof amplitude !== 'undefined') {
-      if (!user.isVirtual) {
+      if (typeof amplitude !== 'undefined' && !user.isVirtual) {
         const revenue = new amplitude.Revenue()
           .setProductId(`${contract.underlying}.${contract.contract_type}`)
-          .setPrice(-profit)
-          .setRevenueType((profit < 0) ? 'loss' : 'win')
-        amplitude.getInstance().logRevenueV2(revenue, {
-          contract,
-        })
+          .setPrice(-profit).setRevenueType((profit < 0) ? 'loss' : 'win')
+
+        amplitude.getInstance().logRevenueV2(revenue, { contract })
       }
-    }
-  })
+    })
+  }
+
+  if (trackJs) {
+    trackJs.configure({ userId })
+  }
 }
