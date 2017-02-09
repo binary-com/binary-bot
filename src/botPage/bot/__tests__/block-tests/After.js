8dd5ad2f91d8012b4a99450092e816c213c114b5
Amin Marashi <marashi.s.amin@gmail.com>
2017-02-09 11:17:15 +0800

    Break BlocksInterface into separeate tests

---
+++ src/botPage/bot/__tests__/block-tests/After.js
@@ -0,0 +1,53 @@
+import { expect } from 'chai'
+import { createJsi, header, trade, footer } from './shared'
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000 * 2
+
+describe('After Purchase Blocks', () => {
+  let value
+
+  const jsi = createJsi()
+
+  beforeAll(done => {
+    jsi.run(`
+      ${header}
+      function tradeAgain() {
+        if (!again) {
+          again = true
+          return true;
+        }
+        return false;
+      }
+      while (true) {
+        ${trade}
+        watch('before')
+        Bot.purchase('CALL')
+        while (testScope(context = watch('during'), 'during')) {
+          Bot.sellAtMarket();
+        }
+        result.isWin = Bot.isResult('win');
+        result.detail = Bot.readDetails(1);
+        if (!tradeAgain()) {
+          break;
+        }
+      }
+      ${footer}
+    `).then(v => {
+      value = v
+      done()
+    })
+  })
+
+  it('before purchase api', () => {
+    const expectedResultTypes = [
+      'boolean', // is result win
+      'string', // statement
+    ]
+
+    const { result } = value
+    const resultTypes = Object.keys(result).map(k => typeof result[k])
+
+    expect(resultTypes).deep.equal(expectedResultTypes)
+  })
+})
+
