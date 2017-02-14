3637378d9528f01db3ac514dce6da45f68506bc4
Amin Marashi <marashi.s.amin@gmail.com>
2017-02-14 10:45:26 +0800

    Add notify to tests

--- src/botPage/bot/__tests__/block-tests/tools-test/Misc.js
+++ src/botPage/bot/__tests__/block-tests/tools-test/Misc.js
@@ -1,16 +1,23 @@
 import { expect } from 'chai'
 import { createJsi, header, trade, footer } from '../shared'
+import { observer as viewObserver } from '../../../../common/shared'
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000 * 2
 
 describe('Misc. tools', () => {
   let value
   const jsi = createJsi()
+  const observed = {}
+
+  viewObserver.register('Notify', notify => (observed.notify = notify))
+  viewObserver.register('NotifyError', notifyError => (observed.notifyError = notifyError))
 
   beforeAll(done => {
     jsi.run(`
       ${header}
       ${trade}
+        Bot.notify('Test', 'info')
+        Bot.notifyError('Test2')
         result.totalRuns = Bot.getTotalRuns();
       ${footer}
     `).then(v => {
@@ -23,5 +30,12 @@ describe('Misc. tools', () => {
     const { result: { totalRuns } } = value
 
     expect(totalRuns).equal(0)
+  })
+
+  it('Notify', () => {
+    const { notify, notifyError } = observed
+
+    expect(notify).deep.equal(['Test', 'info'])
+    expect(notifyError).deep.equal(['Test2'])
   })
 })
