692465997a6349cd42c2d655662e02818353d0f1
Amin Marashi <marashi.s.amin@gmail.com>
2017-02-14 10:21:42 +0800

    fix eslint in direction

--- src/botPage/view/blockly/blocks/ticks/direction.js
+++ src/botPage/view/blockly/blocks/ticks/direction.js
@@ -11,7 +11,7 @@ Blockly.Blocks.direction = {
     this.setTooltip(translate("Returns the tick direction received by a before purchase block, its value could be 'up' if the tick is more than before, 'down' if less than before and empty ('') if the tick is equal to the previous tick"))
     this.setHelpUrl('https://github.com/binary-com/binary-bot/wiki')
   },
-  onchange: function onchange(ev) {
+  onchange: function onchange() {
     disable(this, translate('Direction block is deprecated, please use the check direction block instead'))
   },
 }
