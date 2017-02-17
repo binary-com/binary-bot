d5ef6f1fa4169185dc90710635d2d942764363b2
Amin Marashi <marashi.s.amin@gmail.com>
2017-02-17 09:19:03 +0800

    Add appId to API

--- src/botPage/view/shared.js
+++ src/botPage/view/shared.js
@@ -1,7 +1,11 @@
 import { LiveApi } from 'binary-live-api'
+import { get as getStorage } from 'binary-common-utils/lib/storageManager'
 import _Symbol from '../common/symbolApi'
 
-let api = new LiveApi()
+let api = new LiveApi({
+  language: getStorage('lang') || 'en',
+  appId: getStorage('appId') || 1,
+})
 
 export const symbolApi = new _Symbol(api)
 
