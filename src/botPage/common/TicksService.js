8264422fda546c9a2b949aacbb0bbf76073e1621
Amin Marashi <marashi.s.amin@gmail.com>
2017-03-03 15:30:13 +0800

    Refactor ticks Service

--- src/botPage/common/TicksService.js
+++ src/botPage/common/TicksService.js
@@ -38,50 +38,60 @@ export default class TicksService {
     this.ohlcListeners = new Map()
     this.observe()
   }
-  request(symbol, granularity) {
+  request(symbol, granularity, subscribe) {
     const style = getType(granularity)
 
     if (style === 'ticks' && this.ticks.has(symbol)) {
       return Promise.resolve(this.ticks.get(symbol))
     }
 
-    if (style === 'candles' && this.candles.has(symbol) &&
-      this.candles.get(symbol).has(granularity)) {
-      return Promise.resolve(this.candles.get(symbol).get(granularity))
+    if (style === 'candles' && this.candles.hasIn(['symbol', 'granularity'])) {
+      return Promise.resolve(this.candles.getIn(['symbol', 'granularity']))
     }
 
     return new Promise((resolve, reject) => {
       this.api.getTickHistory(symbol,
-        { subscribe: 1, end: 'latest', count: 1000, granularity, style })
+        { subscribe, end: 'latest', count: 1000, granularity, style })
           .then(r => {
             if (style === 'ticks') {
-              this.ticks = this.ticks.set(symbol, historyToTicks(r.history))
+              const ticks = historyToTicks(r.history)
 
-              resolve(this.ticks.get(symbol))
+              if (subscribe) {
+                this.ticks = this.ticks.set(symbol, ticks)
+              }
+
+              resolve(ticks)
             } else {
-              this.candles = this.candles.setIn([symbol, granularity],
-                parseCandles(r.candles))
+              const candles = parseCandles(r.candles)
 
-              resolve(this.candles.getIn([symbol, granularity]))
+              if (subscribe) {
+                this.candles = this.candles.setIn([symbol, granularity], candles)
+              }
+
+              resolve(candles)
             }
           })
         .catch(reject)
     })
   }
-  getHistory(...args) {
-    return this.request(...args)
+  getHistory(symbol, granularity) {
+    return this.request(symbol, granularity)
   }
-  getLast(...args) {
+  getLast(symbol, granularity) {
     return new Promise((resolve, reject) => {
-      this.request(...args).then(t => resolve(getLast(t)), reject)
+      this.request(symbol, granularity).then(t => resolve(getLast(t)), reject)
     })
   }
   monitor(symbol, ...args) {
     const type = getType(args.length === 2)
+
     const callback = getLast(args)
+
+    const granularity = (type === 'ticks' ? undefined : args[0])
+
     const key = getUUID()
 
-    this.request(symbol, ...args.slice(0, -1))
+    this.request(symbol, granularity, 1)
 
     if (type === 'ticks') {
       this.tickListeners = this.tickListeners.setIn([symbol, key], callback)
