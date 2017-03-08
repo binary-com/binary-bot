ad4f334ebc9c558f357dd0d8a7cb7f898bdf4dc4
Amin Marashi <marashi.s.amin@gmail.com>
2017-03-08 12:22:21 +0800

    Fix follow ticks to match the new ticks service

--- src/botPage/bot/TradeEngine/FollowTicks.js
+++ src/botPage/bot/TradeEngine/FollowTicks.js
@@ -4,16 +4,18 @@ export default Engine => class OpenContract extends Engine {
     this.tickListener = {}
   }
   followTicks(symbol) {
+    const callback = () => {
+      if (!this.isPurchaseStarted && this.checkReady()) {
+        this.execContext('before')
+      }
+    }
+
     if (this.tickListener.symbol !== symbol) {
       const { ticksService } = this.$scope
 
-      ticksService.stopMonitor(this.tickListener.symbol, this.tickListener.key)
+      ticksService.stopMonitor(this.tickListener)
 
-      const key = ticksService.monitor(symbol, () => {
-        if (!this.isPurchaseStarted && this.checkReady()) {
-          this.execContext('before')
-        }
-      })
+      const key = ticksService.monitor({ symbol, callback })
 
       this.tickListener = { key, symbol }
     }
