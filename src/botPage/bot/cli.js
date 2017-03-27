6b4bd6c9548b5a763cd3258060cb7ad798c71821
Amin Marashi <marashi.s.amin@gmail.com>
2017-03-27 16:13:01 +0800

    Add help and version to cli

--- src/botPage/bot/cli.js
+++ src/botPage/bot/cli.js
@@ -2,11 +2,14 @@ import 'babel-polyfill'
 import Observer, { observer as globalObserver } from 'binary-common-utils/lib/observer'
 import fs from 'fs'
 import readline from 'readline'
-import minimist from 'minimist'
+import program from 'commander'
 import WebSocket from 'ws'
 import { LiveApi } from 'binary-live-api'
 import Interpreter from './Interpreter'
 import TicksService from '../common/TicksService'
+
+const log = (...args) =>
+  console.log(`${new Date().toLocaleTimeString()}:`, ...args) // eslint-disable-line no-console
 
 export const createScope = () => {
   const observer = new Observer()
@@ -22,21 +25,28 @@ export const createScope = () => {
 
 export const createInterpreter = () => new Interpreter(createScope())
 
-const args = minimist(process.argv.slice(2))
+let filename
+
+program.version('7.0.4')
+  .usage('[filename]')
+  .arguments('[filename]')
+  .action(fn => {
+    filename = fn
+  })
+  .parse(process.argv)
 
 const lineReader = readline.createInterface({
-  input: args._.length ?
-    fs.createReadStream(args._[0]) :
-    process.stdin,
+  input: filename ? fs.createReadStream(filename) : process.stdin,
 })
 
 let code = ''
 
-lineReader.on('line', line => (code += `${line}\n`))
-
 const interpreter = createInterpreter()
 
-globalObserver.register('Error', e => console.log(e)) // eslint-disable-line no-console
+globalObserver.register('Error', e => log(e)) // eslint-disable-line no-console
 
-lineReader.on('close', () => interpreter.run(code)
-  .then(v => console.log(v.data), e => console.log(e))) // eslint-disable-line no-console
+lineReader
+  .on('line', line => (code += `${line}\n`))
+  .on('error', e => log(e))
+  .on('close', () => interpreter.run(code)
+    .then(v => log(v.data)).catch(e => log(e)))
