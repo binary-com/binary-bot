d1a36e4e75831936c34f7d61df404b18410fe4d7
Amin Marashi <marashi.s.amin@gmail.com>
2017-03-30 14:12:23 +0800

    Fix backward compatibility of markets

--- src/botPage/view/blockly/blocks/trade/markets.js
+++ src/botPage/view/blockly/blocks/trade/markets.js
@@ -20,6 +20,7 @@ export default () => {
           const market = Blockly.mainWorkspace.newBlock('market')
           market.initSvg()
           market.render()
+          const symbol = symbols[this.type]
           if (parent) {
             if (parent.nextConnection) {
               parent.nextConnection.connect(market.previousConnection)
@@ -28,21 +29,24 @@ export default () => {
               statementConnection.connect(market.previousConnection)
             }
           }
-          const symbol = symbols[this.type]
-          market.setFieldValue(symbol.market, 'MARKET_LIST')
-          market.setFieldValue(symbol.submarket, 'SUBMARKET_LIST')
-          market.setFieldValue(symbol.symbol, 'SYMBOL_LIST')
+          if (parent) {
+            parent.setFieldValue(symbol.market, 'MARKET_LIST')
+            parent.setFieldValue(symbol.submarket, 'SUBMARKET_LIST')
+            parent.setFieldValue(symbol.symbol, 'SYMBOL_LIST')
+          }
           if (this.getChildren().length) {
             const condition = this.getChildren()[0]
-            const tradeType = condition.type
-            const categories = config.conditionsCategory
-            Object.keys(categories).forEach(cat => {
-              if (categories[cat].indexOf(tradeType) >= 0) {
-                market.setFieldValue(cat, 'TRADETYPECAT_LIST')
-                market.setFieldValue(tradeType, 'TRADETYPE_LIST')
-                updateInputList(market)
-              }
-            })
+            if (parent && !parent.nextConnection) {
+              const tradeType = condition.type
+              const categories = config.conditionsCategory
+              Object.keys(categories).forEach(cat => {
+                if (categories[cat].indexOf(tradeType) >= 0) {
+                  parent.setFieldValue(cat, 'TRADETYPECAT_LIST')
+                  parent.setFieldValue(tradeType, 'TRADETYPE_LIST')
+                  updateInputList(market)
+                }
+              })
+            }
             condition.inputList.forEach(input => {
               if (input.connection && input.connection.targetConnection) {
                 market.getInput(input.name).connection.connect(input.connection.targetConnection)
