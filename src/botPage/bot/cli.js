e08bedeec758a67d664fe38a4b4e77f2f357a8f6
Amin Marashi <marashi.s.amin@gmail.com>
2017-03-31 14:00:55 +0800

    8.0.3

--- src/botPage/bot/cli.js
+++ src/botPage/bot/cli.js
@@ -27,7 +27,7 @@ export const createInterpreter = () => new Interpreter(createScope())
 
 let filename
 
-program.version('8.0.2')
+program.version('8.0.3')
   .usage('[filename]')
   .arguments('[filename]')
   .action(fn => {
