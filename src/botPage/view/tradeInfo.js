09cc263658f6f348a19fcdbae06a4eda486a5e4e
Amin Marashi <marashi.s.amin@gmail.com>
2017-04-17 17:59:27 +0800

    Convert trade info to react

--- src/botPage/view/tradeInfo.js
+++ src/botPage/view/tradeInfo.js
@@ -1,4 +1,6 @@
-const maxTradeCount = 50;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TradeTable from './TradeTable';
 
 const tradeInfoSkel = {
   totalRuns: 0,
@@ -38,42 +40,10 @@ export default class TradeInfo {
     this.tradeInfo = { ...this.tradeInfo, ...info };
     this.update();
   }
-  addContract(contract) {
-    const number = this.tradeInfo.totalRuns;
-    if (
-      String(contract.transaction_ids.buy) !==
-      $('#tradesDisplay tbody tr:last td:nth-child(2)').text()
-    ) {
-      if (this.tradeCount === maxTradeCount) {
-        $('#tradesDisplay tbody tr:first').insertAfter(
-          $('#tradesDisplay tbody tr:last'),
-        );
-      } else {
-        this.tradeCount += 1;
-        $('#tradesDisplay tbody').append(
-          '<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>',
-        );
-        $('.table-scroll').scrollTop($('.table-scroll')[0].scrollHeight);
-      }
-    }
-
-    const profit = 'sell_price' in contract
-      ? +(Number(contract.sell_price) - Number(contract.buy_price)).toFixed(2)
-      : '';
-    const profitColor = profit < 0 ? 'red' : 'green';
-
-    $('#tradesDisplay tbody tr:last td:nth-child(1)').text(number);
-    $('#tradesDisplay tbody tr:last td:nth-child(2)').text(
-      contract.transaction_ids.buy,
+  addContract(trade) {
+    ReactDOM.render(
+      <TradeTable trade={{ reference: trade.transaction_ids.buy, ...trade }} />,
+      $('#tradeInfo')[0],
     );
-    $('#tradesDisplay tbody tr:last td:nth-child(3)').text(
-      contract.contract_type,
-    );
-    $('#tradesDisplay tbody tr:last td:nth-child(4)').text(contract.entry_tick);
-    $('#tradesDisplay tbody tr:last td:nth-child(5)').text(contract.exit_tick);
-    $('#tradesDisplay tbody tr:last td:nth-child(6)').text(contract.buy_price);
-    $('#tradesDisplay tbody tr:last td:nth-child(7)').text(contract.sell_price);
-    $('#tradesDisplay tbody tr:last td:nth-child(8)').text(profit);
-    $('#tradesDisplay tbody tr:last td:nth-child(8)').css('color', profitColor);
   }
 }
