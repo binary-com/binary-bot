f34171b56a05205e8e2fb7e61bc717f919c03980
Amin Marashi <marashi.s.amin@gmail.com>
2017-04-17 17:59:13 +0800

    Use prop-types package

--- src/botPage/view/Dialogs/DialogComponent.js
+++ src/botPage/view/Dialogs/DialogComponent.js
@@ -1,4 +1,5 @@
-import React, { PureComponent, PropTypes } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import 'jquery-ui/ui/widgets/dialog';
 
 export default class PanelComponent extends PureComponent {
