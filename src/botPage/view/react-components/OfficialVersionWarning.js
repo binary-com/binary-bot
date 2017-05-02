3c4f4451333c4a29d7865e579f7955635b00cade
Amin Marashi <marashi.s.amin@gmail.com>
2017-05-02 12:01:36 +0800

    Prettify official version warning

--- src/botPage/view/react-components/OfficialVersionWarning.js
+++ src/botPage/view/react-components/OfficialVersionWarning.js
@@ -1,38 +1,32 @@
-import React, { PropTypes } from 'react'
-import { translate } from '../../../common/i18n'
+import React, { PropTypes } from 'react';
+import { translate } from '../../../common/i18n';
 
 const OfficialVersionWarning = ({ show }) =>
-  (show
-    ? <div
-        style={{
-          bottom: '0px',
-          position: 'fixed',
-          zIndex: 9999,
-          background: '#c03',
-          color: 'white',
-          width: '100%',
-          textAlign: 'center',
-          fontSize: '1rem',
-          lineHeight: '25px',
-        }}
-    >
-        <div id="end-note">
-          {translate(
-            'This is not an official version of Binary Bot, use at your own risk.',
-          )}
-          <a
-            style={{ fontSize: '1rem', textDecoration: 'underline' }}
-            href="https://bot.binary.com/bot.html"
+    (show
+        ? <div
+              style={{
+                  bottom    : '0px',
+                  position  : 'fixed',
+                  zIndex    : 9999,
+                  background: '#c03',
+                  color     : 'white',
+                  width     : '100%',
+                  textAlign : 'center',
+                  fontSize  : '1rem',
+                  lineHeight: '25px',
+              }}
           >
-            {translate('Official Version')}
-          </a>
-        </div>
-      </div>
-    : null)
+              <div id="end-note">
+                  {translate('This is not an official version of Binary Bot, use at your own risk.')}
+                  <a style={{ fontSize: '1rem', textDecoration: 'underline' }} href="https://bot.binary.com/bot.html">
+                      {translate('Official Version')}
+                  </a>
+              </div>
+          </div>
+        : null);
 
 OfficialVersionWarning.propTypes = {
-  show: PropTypes.bool.isRequired,
-}
+    show: PropTypes.bool.isRequired,
+};
 
-export default OfficialVersionWarning
-
+export default OfficialVersionWarning;
