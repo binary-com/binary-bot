d69fd026edf1e9d3474d3fe67e0ec246641c814f
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-15 14:34:28 +0800

    Handle not opening the notification panel before first log

--- src/botPage/view/LogTable/index.js
+++ src/botPage/view/LogTable/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LogTable from './LogTable';
 
+// for the first props
+ReactDOM.render(<LogTable log={{}} />, $('#logTable')[0]);
+
 export default function addToNotificationPanel(log) {
     ReactDOM.render(<LogTable log={log} />, $('#logTable')[0]);
 }
