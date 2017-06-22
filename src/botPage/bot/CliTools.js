d491a577eef5d47e6981ea7028c5d49ef4fe8cff
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-22 18:01:46 +0800

    Fix webpack config web for external websocket

--- src/botPage/bot/CliTools.js
+++ src/botPage/bot/CliTools.js
@@ -1,18 +1,16 @@
 import Observer from 'binary-common-utils/lib/observer';
 import { LiveApi } from 'binary-live-api';
 import { get as getStorage } from 'binary-common-utils/lib/storageManager';
+import websocket from 'ws';
 import Interpreter from './Interpreter';
 import TicksService from '../common/TicksService';
-
-// eslint-disable-next-line no-new-func
-const isBrowser = new Function('try {return this===window;}catch(e){ return false;}');
 
 export const createApi = () =>
     new LiveApi({
         // eslint-disable-next-line global-require
-        websocket: isBrowser() ? require('ws') : undefined,
-        language : getStorage('lang') || 'en',
-        appId    : getStorage('appId') || 1,
+        websocket,
+        language: getStorage('lang') || 'en',
+        appId   : getStorage('appId') || 1,
     });
 
 export const createScope = () => {
--- webpack.config.web.js
+++ webpack.config.web.js
@@ -1,55 +1,57 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 
-const production = process.env.NODE_ENV === 'production'
+const production = process.env.NODE_ENV === 'production';
 
 const plugins = [
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    'window.jQuery': 'jquery',
-  }),
-]
+    new webpack.ProvidePlugin({
+        $              : 'jquery',
+        jQuery         : 'jquery',
+        'window.jQuery': 'jquery',
+    }),
+];
 
-const productionPlugins = production ? [
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify('production'),
-    },
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    include: /\.js$/,
-    minimize: true,
-    sourceMap: true,
-    compress: {
-      warnings: false,
-    },
-  }),
-] : []
+const productionPlugins = production
+    ? [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+            },
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            include  : /\.js$/,
+            minimize : true,
+            sourceMap: true,
+            compress : {
+                warnings: false,
+            },
+        }),
+    ]
+    : [];
 
 module.exports = {
-  entry: {
-    bot: path.join(__dirname, 'src', 'botPage', 'view'),
-    index: path.join(__dirname, 'src', 'indexPage'),
-  },
-  output: {
-    filename: production ? '[name].min.js' : '[name].js',
-    sourceMapFilename: production ? '[name].min.js.map' : '[name].js.map',
-  },
-  devtool: 'source-map',
-  watch: !production,
-  target: 'web',
-  externals: {
-    ws: 'ws',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-    ],
-  },
-  plugins: plugins.concat(productionPlugins),
-}
+    entry: {
+        bot  : path.join(__dirname, 'src', 'botPage', 'view'),
+        index: path.join(__dirname, 'src', 'indexPage'),
+    },
+    output: {
+        filename         : production ? '[name].min.js' : '[name].js',
+        sourceMapFilename: production ? '[name].min.js.map' : '[name].js.map',
+    },
+    devtool  : 'source-map',
+    watch    : !production,
+    target   : 'web',
+    externals: {
+        ws: 'WebSocket',
+    },
+    module: {
+        rules: [
+            {
+                test   : /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use    : 'babel-loader',
+            },
+        ],
+    },
+    plugins: plugins.concat(productionPlugins),
+};
