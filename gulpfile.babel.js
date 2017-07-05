e44a1338522d8e9337e05ccac759ca25f92ff9ae
Amin Marashi <marashi.s.amin@gmail.com>
2017-07-05 12:02:59 +0800

    Add old to gh-pages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,45 +1,59 @@
-const gulp = require('gulp')
-const watch = require('gulp-watch')
-const ghPages = require('gulp-gh-pages')
-const connect = require('gulp-connect')
-const open = require('gulp-open')
-require('./gulp/i18n')
-require('./gulp/build')
-require('./gulp/plato')
-
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const ghPages = require('gulp-gh-pages');
+const connect = require('gulp-connect');
+const open = require('gulp-open');
+require('./gulp/i18n');
+require('./gulp/build');
+require('./gulp/plato');
 
 gulp.task('connect', () => {
-  connect.server({
-    root: 'www',
-    port: 8080,
-    livereload: true,
-  });
+    connect.server({
+        root      : 'www',
+        port      : 8080,
+        livereload: true,
+    });
 });
 
-gulp.task('open', () => gulp.src('www/index.html')
-  .pipe(open({
-    uri: 'http://localhost:8080/',
-  })));
+gulp.task('open', () =>
+    gulp.src('www/index.html').pipe(
+        open({
+            uri: 'http://localhost:8080/',
+        })
+    )
+);
 
 gulp.task('serve', ['open', 'connect'], () => {
-  watch(['www/*.html'])
-    .pipe(connect.reload());
+    watch(['www/*.html']).pipe(connect.reload());
 });
 
-gulp.task('deploy', ['build-min'],
-  () => gulp.src(['404.md', 'LICENSE', 'README.md', 'CNAME', './www/**', './beta/**', './translation/**'])
-  .pipe(ghPages()));
+gulp.task('deploy', ['build-min'], () =>
+    gulp
+        .src(['404.md', 'LICENSE', 'README.md', 'CNAME', './www/**', './beta/**', './translation/**', './old/**'])
+        .pipe(ghPages())
+);
 
-gulp.task('test-deploy', ['build-min', 'serve'], () => {
-});
+gulp.task('test-deploy', ['build-min', 'serve'], () => {});
 
-gulp.task('watch-static', () => gulp.watch(['static/xml/**/*', 'static/*.html', 'static/css/*.scss'], {
-  debounceTimeout: 1000,
-}, ['build-dev-static']));
+gulp.task('watch-static', () =>
+    gulp.watch(
+        ['static/xml/**/*', 'static/*.html', 'static/css/*.scss'],
+        {
+            debounceTimeout: 1000,
+        },
+        ['build-dev-static']
+    )
+);
 
-gulp.task('watch-html', () => gulp.watch(['templates/**/*'], {
-  debounceTimeout: 1000,
-}, ['build-dev-html']));
+gulp.task('watch-html', () =>
+    gulp.watch(
+        ['templates/**/*'],
+        {
+            debounceTimeout: 1000,
+        },
+        ['build-dev-html']
+    )
+);
 
 gulp.task('watch', ['serve', 'build', 'watch-static', 'watch-html']);
 
