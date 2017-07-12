a8d9d1c25a1f2f65c31530ef5cb44ae2903a01bc
Amin Marashi <marashi.s.amin@gmail.com>
2017-07-12 15:41:43 +0800

    Fix typo

--- src/botPage/view/blockly/blocks/ticks/tick_analysis.js
+++ src/botPage/view/blockly/blocks/ticks/tick_analysis.js
@@ -6,7 +6,7 @@ Blockly.Blocks.tick_analysis = {
         this.appendDummyInput().appendField(translate('This block is called on every tick'));
         this.appendStatementInput('TICKANALYSIS_STACK').setCheck(null);
         this.setColour('#fef1cf');
-        this.setTooltip(translate('You can use this block to analyze the ticks comming regardless of your trades'));
+        this.setTooltip(translate('You can use this block to analyze the ticks, regardless of your trades'));
         this.setHelpUrl('https://github.com/binary-com/binary-bot/wiki');
     },
 };
