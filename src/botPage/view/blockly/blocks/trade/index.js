ee04d78bdf6faf747a4b8448e41ad32826cf89a7
Gokula Krishna <gokula@binary.com>
2018-01-08 15:02:06 +0800

    Contract type validation logic fix with return

--- src/botPage/view/blockly/blocks/trade/index.js
+++ src/botPage/view/blockly/blocks/trade/index.js
@@ -59,13 +59,10 @@ const replaceInitializationBlocks = (trade, ev) => {
 };
 
 const setDefaultFields = (trade, parentFieldName) => {
-    if (Object.keys(dependentFieldMapping).includes(ev.name)) {
-        const childFieldName = dependentFieldMapping[parentFieldName];
-        const [[, defaultValue]] = fieldGeneratorMapping[childFieldName](trade)();
-        trade.setFieldValue(defaultValue, childFieldName);
-    } else {
-        
-    }
+    if (!Object.keys(dependentFieldMapping).includes(ev.name)) return;
+    const childFieldName = dependentFieldMapping[parentFieldName];
+    const [[, defaultValue]] = fieldGeneratorMapping[childFieldName](trade)();
+    trade.setFieldValue(defaultValue, childFieldName);
 };
 
 const resetTradeFields = (trade, ev) => {
