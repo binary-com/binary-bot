1da154748232e45885b13f8bbb92e635bac1167f
Gokula Krishna <gokula@binary.com>
2018-02-06 15:27:05 +0800

    Introduce login check

--- src/indexPage/index.js
+++ src/indexPage/index.js
@@ -5,14 +5,18 @@ import { load as loadLang } from '../common/lang';
 import '../common/binary-ui/dropdown';
 import endpoint from './endpoint';
 
-if (endpoint()) return;
-if (getTokenList().length) {
-    window.location.pathname = `${window.location.pathname.replace(/\/+$/, '')}/bot.html`;
-} else {
-    loadLang();
-    setDefaultAppId();
-    oauthLogin(() => {
-        $('.show-on-load').show();
-        $('.barspinner').hide();
-    });
-}
+const loginCheck = () => {
+    if (endpoint()) return;
+    if (getTokenList().length) {
+        window.location.pathname = `${window.location.pathname.replace(/\/+$/, '')}/bot.html`;
+    } else {
+        loadLang();
+        setDefaultAppId();
+        oauthLogin(() => {
+            $('.show-on-load').show();
+            $('.barspinner').hide();
+        });
+    }
+};
+
+loginCheck();
