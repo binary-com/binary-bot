ae6ec80bacaaa3f038129c00910202fceee456a7
Gokula Krishna <gokula@binary.com>
2018-06-07 14:56:09 +0800

    Make active token key as constant

--- src/botPage/view/View.js
+++ src/botPage/view/View.js
@@ -23,7 +23,7 @@ import Tour from './tour';
 import OfficialVersionWarning from './react-components/OfficialVersionWarning';
 import LogTable from './LogTable';
 import TradeInfoPanel from './TradeInfoPanel';
-import { logoutAllTokens, getOAuthURL, generateLiveApiInstance } from '../../common/appId';
+import { logoutAllTokens, getOAuthURL, generateLiveApiInstance, AppConstants } from '../../common/appId';
 import { updateConfigCurrencies } from '../common/const';
 
 let realityCheckTimeout;
@@ -131,7 +131,7 @@ const updateTokenList = () => {
         loginButton.hide();
         accountList.show();
 
-        const activeToken = getActiveToken(tokenList, getStorage('activeToken'));
+        const activeToken = getActiveToken(tokenList, getStorage(AppConstants.STORAGE_ACTIVE_TOKEN));
         addBalanceForToken(activeToken.token);
         if (!('loginInfo' in activeToken)) {
             removeAllTokens();
@@ -276,7 +276,7 @@ export default class View {
         };
 
         const clearActiveTokens = () => {
-            setStorage('activeToken', '');
+            setStorage(AppConstants.STORAGE_ACTIVE_TOKEN, '');
         };
 
         $('.panelExitButton').click(function onClick() {
@@ -427,7 +427,7 @@ export default class View {
         });
 
         $('.login-id-list').on('click', 'a', e => {
-            setStorage('activeToken', $(e.currentTarget).attr('value'));
+            setStorage(AppConstants.STORAGE_ACTIVE_TOKEN, $(e.currentTarget).attr('value'));
             location.reload();
         });
 
