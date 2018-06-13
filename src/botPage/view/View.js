c0a7601fbd945069b0cbed36bcf3dc31878b7904
Gokula Krishna <gokula@binary.com>
2018-06-13 15:22:16 +0800

    Remove unused code

--- src/botPage/view/View.js
+++ src/botPage/view/View.js
@@ -106,7 +106,6 @@ const resetRealityCheck = token => {
 
 const limits = new Limits();
 const saveDialog = new Save();
-// const chart = new Chart();
 
 const updateTokenList = () => {
     const tokenList = getTokenList();
