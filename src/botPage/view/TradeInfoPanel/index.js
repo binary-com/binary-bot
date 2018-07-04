bd3a05719b120b631e4d23f93c8bfe4f5d5efb75
Gokula Krishna <gokula@binary.com>
2018-07-04 17:21:22 +0800

    Initial commit for current trade status animation

--- src/botPage/view/TradeInfoPanel/index.js
+++ src/botPage/view/TradeInfoPanel/index.js
@@ -5,6 +5,55 @@ import { warningText, tradePanelAccount } from '../style';
 import Summary from './Summary';
 import TradeTable from './TradeTable';
 
+class AnimateTrade extends Component {
+    render() {
+        return (
+            <div>
+                <div id="current-trade-status">
+                    <span className="circle-wrapper">
+                        <span className="static-circle" />
+                        <span className="dynamic-circle" />
+                    </span>
+                    <span className="line">
+                        <span className="progress-bar" />
+                    </span>
+                    <span className="circle-wrapper">
+                        <span className="static-circle" />
+                        <span className="dynamic-circle" />
+                    </span>
+                    <span className="line">
+                        <span className="progress-bar" />
+                    </span>
+                    <span className="circle-wrapper">
+                        <span className="static-circle" />
+                        <span className="dynamic-circle" />
+                    </span>
+                </div>
+            </div>
+        );
+    }
+}
+
+const activateStage = status => {
+    if (status === 'contract.purchase_sent') {
+        $('.circle-wrapper')
+            .removeClass('active')
+            .removeClass('complete');
+        $('.line').removeClass('active');
+        $('.circle-wrapper:eq(0)').addClass('active');
+    } else if (status === 'contract.purchase_recieved') {
+        $('.circle-wrapper:eq(0)').removeClass('active');
+        $('.circle-wrapper:eq(0)').addClass('complete');
+        $('.line:eq(0)').addClass('active');
+        $('.circle-wrapper:eq(1)').addClass('active');
+    } else if (status === 'contract.sold') {
+        $('.circle-wrapper:eq(1)').removeClass('active');
+        $('.circle-wrapper:eq(1)').addClass('complete');
+        $('.line:eq(1)').addClass('active');
+        $('.circle-wrapper:eq(2)').addClass('active');
+    }
+};
+
 export default class TradeInfoPanel extends Component {
     constructor() {
         super();
@@ -23,6 +72,7 @@ export default class TradeInfoPanel extends Component {
         });
         globalObserver.register('contract.status', contractStatus => {
             this.setState({ contractStatus });
+            activateStage(contractStatus);
         });
     }
     render() {
@@ -54,6 +104,11 @@ export default class TradeInfoPanel extends Component {
 
                         <Summary accountID={accountID} />
                     </div>
+                    <h3>
+                        <span>{translate('Current trade status')}</span>
+                    </h3>
+
+                    <AnimateTrade />
                     <div>
                         <h3>
                             <span>{translate('Trades')}</span>
