ad46f72930c3d9f28753eb98ac1b33e388ebf8dd
Aaron Imming <aaron@binary.com>
2018-11-13 14:23:22 +0800

    fix logo

--- src/indexPage/react-components/logo.jsx
+++ src/indexPage/react-components/logo.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import { createUrl } from '../../common/utils/tools';
 
 const Logo = () => (
-    <div>
-        <a href={createUrl('', '', false)} target="blank" id="logo">
-            <div class="logo-parent">
-                <div class="logo">
-                    <img class="responsive" src={'image/symbol.svg'} alt="Binary-logo" />
-                </div>
-                <div class="binary-logo-text">
-                    <img class="responsive" src={'image/type.svg'} alt="Binary-logo" />
-                </div>
+    <a href={createUrl('', '', false)} target="blank" id="logo">
+        <div className="logo-parent">
+            <div className="logo">
+                <img className="responsive" src={'image/symbol.svg'} alt="Binary-logo" />
             </div>
-        </a>
-    </div>
+            <div className="binary-logo-text">
+                <img className="responsive" src={'image/type.svg'} alt="Binary-logo" />
+            </div>
+        </div>
+    </a>
 );
 export default Logo;
