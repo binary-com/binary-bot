ddedcd2eaafd5c9486d12a6c896ecb34d311efca
Gokula Krishna <gokula@binary.com>
2018-12-03 10:24:54 +0800

    Restructure _Symbol class: decouple fetch

--- src/botPage/common/symbolApi/index.js
+++ src/botPage/common/symbolApi/index.js
@@ -40,8 +40,10 @@ const getCategoryForCondition = condition =>
     );
 
 export default class _Symbol {
-    constructor(api) {
+    setApi(api) {
         this.api = api;
+    }
+    fetch() {
         this.initPromise = new Promise(resolve => {
             this.api.getActiveSymbolsBrief().then(r => {
                 this.activeSymbols = new ActiveSymbols(r.active_symbols);
