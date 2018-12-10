1245813b7e8d9737b2160a05e1ee64c2229b070e
Aaron Imming <aaron@binary.com>
2018-12-10 18:18:34 +0800

    Wrap string in translate()

--- src/botPage/view/TradeInfoPanel/RunButton.js
+++ src/botPage/view/TradeInfoPanel/RunButton.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { translate } from '../../../common/i18n';
 
 const RunButton = () => (
     <div className="summary-toolbox">
         <button title="Run the bot" id="summaryRunButton" className="toolbox-button icon-run" />
         <button
-            title="Stop the bot"
+            title={translate('Stop the bot')}
             id="summaryStopButton"
             className="toolbox-button icon-stop"
             style={{ display: 'none' }}
