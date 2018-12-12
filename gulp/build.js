e706ceb1eb7deec43a68319f87123ab41bcef206
Aaron Imming <aaron@binary.com>
2018-12-12 11:37:25 +0800

    Add mustache var

--- gulp/build.js
+++ gulp/build.js
@@ -7,16 +7,17 @@ require('./bundle');
 require('./webpack');
 
 const getConfig = prefix => ({
-    index     : `<script src="js/${prefix ? getManifest(`index${prefix}.js`) : 'index.js'}"></script>`,
-    bot       : `<script src="js/${prefix ? getManifest(`bot${prefix}.js`) : 'bot.js'}"></script>`,
-    bundle    : `<script src="js/${getManifest('bundle.js')}"></script>`,
-    bundle_css: `<link href="css/${getManifest('bundle.css')}" rel="stylesheet" />`,
-    index_css : `<link href="css/${getManifest('index.css')}" rel="stylesheet" />`,
-    bot_css   : `<link href="css/${getManifest('bot.css')}" rel="stylesheet" />`,
-    head      : 'templates/partials/head.mustache',
-    security  : 'templates/partials/security.mustache',
-    language  : 'templates/partials/language.mustache',
-    loading   : 'templates/partials/loading.mustache',
+    index           : `<script src="js/${prefix ? getManifest(`index${prefix}.js`) : 'index.js'}"></script>`,
+    bot             : `<script src="js/${prefix ? getManifest(`bot${prefix}.js`) : 'bot.js'}"></script>`,
+    bundle          : `<script src="js/${getManifest('bundle.js')}"></script>`,
+    bundle_css      : `<link href="css/${getManifest('bundle.css')}" rel="stylesheet" />`,
+    index_css       : `<link href="css/${getManifest('index.css')}" rel="stylesheet" />`,
+    bot_css         : `<link href="css/${getManifest('bot.css')}" rel="stylesheet" />`,
+    head            : 'templates/partials/head.mustache',
+    security        : 'templates/partials/security.mustache',
+    language        : 'templates/partials/language.mustache',
+    loading         : 'templates/partials/loading.mustache',
+    binary_style_img: 'image/binary-style',
 });
 
 const genHtml = min =>
