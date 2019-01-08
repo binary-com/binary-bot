ddb6f79fcd1128ab0123c9ed18f11d1d61d5635e
Aaron Imming <aaron@binary.com>
2019-01-08 18:00:52 +0800

    Pass each block through removeUnavailableMarkets() and re-order imports

--- src/botPage/view/blockly/index.js
+++ src/botPage/view/blockly/index.js
@@ -1,6 +1,5 @@
-import { observer as globalObserver } from '../../../common/utils/observer';
-import { translate, xml as translateXml } from '../../../common/i18n';
-import createError from '../../common/error';
+import './customBlockly';
+import blocks from './blocks';
 import {
     isMainBlock,
     save,
@@ -12,11 +11,13 @@ import {
     backwardCompatibility,
     fixCollapsedBlocks,
     fixArgumentAttribute,
+    removeUnavailableMarkets,
 } from './utils';
-import blocks from './blocks';
 import Interpreter from '../../bot/Interpreter';
+import createError from '../../common/error';
+import { translate, xml as translateXml } from '../../../common/i18n';
 import { getLanguage } from '../../../common/lang';
-import './customBlockly';
+import { observer as globalObserver } from '../../../common/utils/observer';
 
 const setBeforeUnload = off => {
     if (off) {
@@ -51,7 +52,10 @@ const disposeBlocksWithLoaders = () => {
 const loadWorkspace = xml => {
     Blockly.Events.setGroup('load');
     Blockly.mainWorkspace.clear();
-    Array.from(xml.children).forEach(block => backwardCompatibility(block));
+    Array.from(xml.children).forEach(block => {
+        backwardCompatibility(block);
+        removeUnavailableMarkets(block);
+    });
     fixArgumentAttribute(xml);
     Blockly.Xml.domToWorkspace(xml, Blockly.mainWorkspace);
     addLoadersFirst(xml).then(
