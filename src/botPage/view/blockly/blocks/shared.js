51f6c5f8cb99983446d82dd3e3e61b784d625527
Aaron Imming <aaron@binary.com>
2019-01-18 11:40:32 +0800

    Implement getAvailableDurations() logic

--- src/botPage/view/blockly/blocks/shared.js
+++ src/botPage/view/blockly/blocks/shared.js
@@ -2,6 +2,8 @@ import { translate } from '../../../../common/i18n';
 import config from '../../../common/const';
 import { oppositesToDropdown } from '../utils';
 import { symbolApi } from '../../shared';
+import { get as getStorage, set as setStorage } from '../../../../common/utils/storageManager';
+import { generateLiveApiInstance } from '../../../../common/appId';
 
 let purchaseChoices = [[translate('Click to select'), '']];
 
@@ -10,14 +12,13 @@ export const getPurchaseChoices = () => purchaseChoices;
 const filterPurchaseChoices = (contractType, oppositesName) => {
     const { [oppositesName]: tradeTypes } = config.opposites;
 
-    let tmpPurchaseChoices = tradeTypes.filter(
-        k => (contractType === 'both' ? true : contractType === Object.keys(k)[0])
+    let tmpPurchaseChoices = tradeTypes.filter(k =>
+        contractType === 'both' ? true : contractType === Object.keys(k)[0]
     );
 
     if (!tmpPurchaseChoices.length) {
         tmpPurchaseChoices = tradeTypes;
     }
-
     return oppositesToDropdown(tmpPurchaseChoices);
 };
 
@@ -131,4 +132,90 @@ export const dependentFieldMapping = {
     SUBMARKET_LIST   : 'SYMBOL_LIST',
     SYMBOL_LIST      : 'TRADETYPECAT_LIST',
     TRADETYPECAT_LIST: 'TRADETYPE_LIST',
+};
+
+export const getAvailableDurations = (symbol, selectedContractType) => {
+    const contractsForStore = JSON.parse(getStorage('contractsForStore') || '[]');
+    const defaultDurations = [
+        [translate('Ticks'), 't'],
+        [translate('Seconds'), 's'],
+        [translate('Minutes'), 'm'],
+        [translate('Hours'), 'h'],
+        [translate('Days'), 'd'],
+    ];
+    const getContractsForSymbolFromApi = async underlyingSymbol => {
+        // Refactor this when reducing WS connections
+        let api = generateLiveApiInstance();
+        const response = await api.getContractsForSymbol(underlyingSymbol);
+        const contractsForSymbol = {};
+        if (response.contracts_for) {
+            Object.assign(contractsForSymbol, {
+                symbol   : underlyingSymbol,
+                available: response.contracts_for.available,
+                timestamp: Date.now(),
+            });
+            contractsForStore.push(contractsForSymbol);
+            setStorage('contractsForStore', JSON.stringify(contractsForStore));
+        }
+        api.disconnect();
+        api = null;
+        return contractsForSymbol;
+    };
+    const getDurationsForContract = contractsForSymbol => {
+        if (!contractsForSymbol) return defaultDurations;
+
+        // Resolve contract_category (e.g. risefall = callput)
+        const contractCategory = Object.keys(config.conditionsCategory).find(
+            c => c === selectedContractType || config.conditionsCategory[c].includes(selectedContractType)
+        );
+
+        // Get contracts based on `contract_category` and `barrier_category`
+        const contractsForContractCategory = contractsForSymbol.filter(c => {
+            const meetsBarrierConditions = () => {
+                const conditions = [];
+                Object.keys(config.barrierCategories).some(barrierCategory => {
+                    if (config.barrierCategories[barrierCategory].includes(selectedContractType)) {
+                        conditions.push(c.barrier_category === barrierCategory);
+                    }
+                    return conditions.length;
+                });
+                // If `c.barrier_category` not found fallback to all contracts for durations
+                return !conditions.includes(false);
+            };
+            return c.contract_category === contractCategory && meetsBarrierConditions();
+        });
+
+        const getDurationIndex = input => defaultDurations.findIndex(d => d[1] === input.replace(/\d+/g, ''));
+
+        // Generate list of available durations from filtered contracts
+        const offeredDurations = [];
+        contractsForContractCategory.forEach(c => {
+            const startIndex = getDurationIndex(c.min_contract_duration);
+            const endIndex = getDurationIndex(c.max_contract_duration);
+            defaultDurations.slice(startIndex, endIndex + 1).forEach(duration => {
+                if (!offeredDurations.includes(duration)) {
+                    offeredDurations.push(duration);
+                }
+            });
+        });
+        if (offeredDurations.length) {
+            offeredDurations.sort((a, b) => getDurationIndex(a[1]) - getDurationIndex(b[1]));
+            return offeredDurations;
+        }
+        return config.durationTypes[selectedContractType.toUpperCase()];
+    };
+    // Check if we have local data to get durations from
+    const contractsForSymbol = contractsForStore.find(c => c.symbol === symbol);
+    if (contractsForSymbol) {
+        // If `contracts_for` data is expired, request new data in background but return current cached data
+        if (Math.floor((Date.now() - contractsForSymbol.timestamp) / 1000) > 600) {
+            getContractsForSymbolFromApi(symbol);
+        }
+        return Promise.resolve(getDurationsForContract(contractsForSymbol.available));
+    }
+    return new Promise(resolve => {
+        getContractsForSymbolFromApi(symbol).then(contractsForSymbolFromApi => {
+            resolve(getDurationsForContract(contractsForSymbolFromApi.available));
+        });
+    });
 };
