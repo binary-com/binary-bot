ae9e353362229748d4c5d66210508f768d1d5317
Aaron Imming <aaron@binary.com>
2019-01-23 13:10:41 +0800

    Update 'Indicator values are set correctly'-test

--- src/botPage/bot/__tests__/block-tests/indicators.js
+++ src/botPage/bot/__tests__/block-tests/indicators.js
@@ -4,7 +4,7 @@ import bb, { bollingerBandsArray as bba } from '@binary-com/binary-indicators/li
 import rsi, { relativeStrengthIndexArray as rsia } from '@binary-com/binary-indicators/lib/relativeStrengthIndex';
 import ema, { exponentialMovingAverageArray as emaa } from '@binary-com/binary-indicators/lib/exponentialMovingAverage';
 import sma, { simpleMovingAverageArray as smaa } from '@binary-com/binary-indicators/lib/simpleMovingAverage';
-import macda from '@binary-com/binary-indicators/lib/macd';
+// import macda from '@binary-com/binary-indicators/lib/macd';
 
 import { runAndGetResult } from '../tools';
 
@@ -17,9 +17,9 @@ const bbOption = {
 };
 
 const macdOption = {
-    fastEmaPeriod: 12,
-    slowEmaPeriod: 26,
-    smaPeriod    : 9,
+    fastEmaPeriod  : 12,
+    slowEmaPeriod  : 26,
+    signalEmaPeriod: 9,
 };
 
 const getIndicatorsFromApi = () =>
@@ -68,7 +68,9 @@ describe('Relative Strength Index', () => {
 
     it('Indicator values are set correctly', () => {
         Object.keys(expected).forEach(name => {
-            expect(result[name]).deep.equal(expected[name]);
+            const endResult = JSON.parse(JSON.stringify(result[name]));
+            const expectedResult = JSON.parse(JSON.stringify(expected[name]));
+            expect(endResult).to.deep.equal(expectedResult);
         });
     });
 });
