0bcbe2a07b49163760ff0f2da221597a6a84a902
Aaron Imming <aaron@binary.com>
2019-02-21 12:26:20 +0800

    Filter n/a (sub)markets & symbols, implement logic for getting barriers + prediction from API, refactoring

--- src/botPage/view/blockly/blocks/shared.js
+++ src/botPage/view/blockly/blocks/shared.js
@@ -9,6 +9,7 @@ import {
     getTokenList,
     removeAllTokens,
 } from '../../../../common/utils/storageManager';
+import { observer as globalObserver } from '../../../../common/utils/observer';
 
 let purchaseChoices = [[translate('Click to select'), '']];
 
@@ -95,7 +96,12 @@ fieldGeneratorMapping.SUBMARKET_LIST = block => () => {
         return [['', 'Invalid']];
     }
     const submarkets = getActiveSubMarket(markets[marketName].submarkets);
-    return Object.keys(submarkets).map(e => [submarkets[e].name, e]);
+    return (
+        Object.keys(submarkets)
+            .map(e => [submarkets[e].name, e])
+            // Filter out markets we don't have contracts for
+            .filter(submarket => !['energy'].includes(submarket[1]))
+    );
 };
 
 fieldGeneratorMapping.SYMBOL_LIST = block => () => {
@@ -107,7 +113,12 @@ fieldGeneratorMapping.SYMBOL_LIST = block => () => {
     const marketName = block.getFieldValue('MARKET_LIST');
     const submarkets = getActiveSubMarket(markets[marketName].submarkets);
     const symbols = getActiveSymbols(submarkets[submarketName].symbols);
-    return Object.keys(symbols).map(e => [symbols[e].display, symbols[e].symbol]);
+    return (
+        Object.keys(symbols)
+            .map(e => [symbols[e].display, symbols[e].symbol])
+            // Filter out symbols we don't have contracts for (these symbols have only forward-starting)
+            .filter(symbol => !['frxGBPNOK', 'frxUSDNOK', 'frxUSDNEK'].includes(symbol[1]))
+    );
 };
 
 fieldGeneratorMapping.TRADETYPECAT_LIST = block => () => {
@@ -126,10 +137,14 @@ fieldGeneratorMapping.TRADETYPE_LIST = block => () => {
     if (!tradeTypeCat) {
         return [['', '']];
     }
-    return config.conditionsCategory[tradeTypeCat].map(e => [
-        config.opposites[e.toUpperCase()].map(c => c[Object.keys(c)[0]]).join('/'),
-        e,
-    ]);
+    return (
+        config.conditionsCategory[tradeTypeCat]
+            .map(e => [config.opposites[e.toUpperCase()].map(c => c[Object.keys(c)[0]]).join('/'), e])
+            // Filter out trade types we don't offer
+            .filter(
+                tradeType => !(block.getFieldValue('SUBMARKET_LIST') === 'smart_fx' && tradeType[1] === 'higherlower')
+            )
+    );
 };
 
 export const dependentFieldMapping = {
@@ -139,9 +154,35 @@ export const dependentFieldMapping = {
     TRADETYPECAT_LIST: 'TRADETYPE_LIST',
 };
 
-export const getAvailableDurations = (symbol, selectedContractType) => {
-    const contractsForStore = JSON.parse(getStorage('contractsForStore') || '[]');
-    let tokenList = getTokenList();
+const contractsForStore = JSON.parse(getStorage('contractsForStore') || '[]');
+
+const getContractCategory = input =>
+    Object.keys(config.conditionsCategory).find(c => c === input || config.conditionsCategory[c].includes(input));
+
+const matchesBarrierCategory = (contract, contractCategory) => {
+    const conditions = [];
+    Object.keys(config.barrierCategories).some(barrierCategory => {
+        if (config.barrierCategories[barrierCategory].includes(contractCategory)) {
+            conditions.push(contract.barrier_category === barrierCategory);
+        }
+        return conditions.length;
+    });
+    // If `barrierCategory` for `contractCategory` not found fallback to all contracts
+    return !conditions.includes(false);
+};
+
+const filterContractsByCategory = (contracts, contractCategory, contractType) => {
+    if (!contracts) return [];
+    return contracts.filter(contract => {
+        // We don't offer forward-starting contracts in Binary Bot, remove these
+        if (contract.start_type === 'forward') {
+            return false;
+        }
+        return contract.contract_category === contractCategory && matchesBarrierCategory(contract, contractType);
+    });
+};
+
+export const getDurationsForContracts = (contractsAvailable, selectedContractType) => {
     const defaultDurations = [
         [translate('Ticks'), 't'],
         [translate('Seconds'), 's'],
@@ -149,23 +190,90 @@ export const getAvailableDurations = (symbol, selectedContractType) => {
         [translate('Hours'), 'h'],
         [translate('Days'), 'd'],
     ];
+    const noDurationsAvailable = [{ label: translate('Not available'), unit: 'na', minimum: 0 }];
+    if (!contractsAvailable) return noDurationsAvailable;
 
-    const getContractsForSymbolFromApi = async underlyingSymbol => {
-        // Refactor this when reducing WS connections
-        const api = generateLiveApiInstance();
+    const getMinimumAmount = input => input.replace(/\D/g, '');
+    const getDurationIndex = input => defaultDurations.findIndex(d => d[1] === input.replace(/\d+/g, ''));
 
-        // Try to authorize for accurate contracts response
-        if (tokenList.length) {
-            try {
-                await api.authorize(tokenList[0].token);
-            } catch (e) {
-                removeAllTokens();
-                tokenList = [];
+    const offeredDurations = [];
+
+    const contractsForContractCategory = filterContractsByCategory(
+        contractsAvailable,
+        getContractCategory(selectedContractType),
+        selectedContractType
+    );
+    contractsForContractCategory.forEach(c => {
+        if (!c.min_contract_duration || !c.max_contract_duration) return;
+
+        const startIndex = getDurationIndex(c.min_contract_duration);
+        const endIndex = getDurationIndex(c.max_contract_duration === '1d' ? '24h' : c.max_contract_duration);
+
+        defaultDurations.slice(startIndex, endIndex + 1).forEach((duration, index) => {
+            if (!offeredDurations.find(offeredDuration => offeredDuration.unit === duration[1])) {
+                offeredDurations.push({
+                    label  : duration[0],
+                    unit   : duration[1],
+                    minimum: index === 0 ? getMinimumAmount(c.min_contract_duration) : 1,
+                });
             }
+        });
+    });
+    // If only intraday contracts available remove day-durations
+    if (contractsForContractCategory.every(c => c.expiry_type === 'intraday')) {
+        const dayDurationIndex = offeredDurations.findIndex(d => d[1] === 'd');
+        if (dayDurationIndex !== -1) {
+            offeredDurations.splice(dayDurationIndex, 1);
         }
+    }
+    if (!offeredDurations.length) {
+        return noDurationsAvailable;
+    }
+    return (
+        offeredDurations
+            // Maintain order based on duration unit
+            .sort((a, b) => getDurationIndex(a.unit) - getDurationIndex(b.unit))
+    );
+};
 
+export const haveContractsForSymbol = underlyingSymbol => contractsForStore.find(c => c.symbol === underlyingSymbol);
+
+export const getContractsAvailableForSymbol = async underlyingSymbol => {
+    const contractsForSymbol = haveContractsForSymbol(underlyingSymbol);
+    if (!contractsForSymbol) {
+        const contractsAvailableForSymbol = await getContractsAvailableForSymbolFromApi(underlyingSymbol);
+        return Promise.resolve(contractsAvailableForSymbol.available);
+    }
+    // Different accounts have access to different markets, request and serve new data
+    const tokenList = getTokenList();
+    const isDifferentAccount = () => tokenList.length && contractsForSymbol.accountName !== tokenList[0].accountName;
+    if (isDifferentAccount()) {
+        const contractsAvailableForSymbol = await getContractsAvailableForSymbolFromApi(underlyingSymbol);
+        return Promise.resolve(contractsAvailableForSymbol.available);
+    }
+    // Data expired, serve cached data, but retrieve updated data async
+    const isExpiredData = () => Math.floor((Date.now() - contractsForSymbol.timestamp) / 1000) > 600;
+    if (isExpiredData()) {
+        getContractsAvailableForSymbolFromApi(underlyingSymbol);
+    }
+    // Not a different account or expired, return what we have.
+    return Promise.resolve(contractsForSymbol.available);
+};
+
+export const getContractsAvailableForSymbolFromApi = async underlyingSymbol => {
+    const api = generateLiveApiInstance();
+    let tokenList = getTokenList();
+    if (tokenList.length) {
+        try {
+            await api.authorize(tokenList[0].token);
+        } catch (e) {
+            removeAllTokens();
+            tokenList = [];
+        }
+    }
+    const contractsForSymbol = {};
+    try {
         const response = await api.getContractsForSymbol(underlyingSymbol);
-        const contractsForSymbol = {};
         if (response.contracts_for) {
             Object.assign(contractsForSymbol, {
                 symbol   : underlyingSymbol,
@@ -186,81 +294,97 @@ export const getAvailableDurations = (symbol, selectedContractType) => {
             contractsForStore.push(contractsForSymbol);
             setStorage('contractsForStore', JSON.stringify(contractsForStore));
         }
-        api.disconnect();
-        return contractsForSymbol;
-    };
-    const getDurationsForContract = contractsForSymbol => {
-        if (!contractsForSymbol) return defaultDurations;
-
-        // Resolve contract_category (e.g. risefall = callput)
-        const contractCategory = Object.keys(config.conditionsCategory).find(
-            c => c === selectedContractType || config.conditionsCategory[c].includes(selectedContractType)
-        );
-
-        // Get contracts based on `contract_category` and `barrier_category`
-        const contractsForContractCategory = contractsForSymbol.filter(c => {
-            const meetsBarrierConditions = () => {
-                const conditions = [];
-                Object.keys(config.barrierCategories).some(barrierCategory => {
-                    if (config.barrierCategories[barrierCategory].includes(selectedContractType)) {
-                        conditions.push(c.barrier_category === barrierCategory);
-                    }
-                    return conditions.length;
-                });
-                // If `barrierCategory` for `selectedContractType` not found fallback to all contracts for durations
-                return !conditions.includes(false);
-            };
-            // We don't offer forward-starting contracts in Binary Bot, remove these
-            if (c.start_type === 'forward') {
-                return false;
-            }
-            return c.contract_category === contractCategory && meetsBarrierConditions();
-        });
-
-        const getDurationIndex = input => defaultDurations.findIndex(d => d[1] === input.replace(/\d+/g, ''));
-
-        // Generate list of available durations from filtered contracts
-        const offeredDurations = [];
-        contractsForContractCategory.forEach(c => {
-            const startIndex = getDurationIndex(c.min_contract_duration);
-            const endIndex = getDurationIndex(c.max_contract_duration === '1d' ? '24h' : c.max_contract_duration);
-            defaultDurations.slice(startIndex, endIndex + 1).forEach(duration => {
-                if (!offeredDurations.includes(duration)) {
-                    offeredDurations.push(duration);
-                }
-            });
-        });
-        // If only intraday contracts are available, remove day-durations
-        if (contractsForContractCategory.every(c => c.expiry_type === 'intraday')) {
-            const dayDurationIndex = offeredDurations.findIndex(d => d[1] === 'd');
-            if (dayDurationIndex !== -1) {
-                offeredDurations.splice(dayDurationIndex, 1);
-            }
+    } catch (e) {
+        if (window.trackJs) {
+            trackJs.addMetadata('getContractsAvailableForSymbolFromApi Error', e.message);
         }
-        offeredDurations.sort((a, b) => getDurationIndex(a[1]) - getDurationIndex(b[1]));
-        return offeredDurations;
-    };
-
-    const getFreshContractsFor = () =>
-        new Promise(resolve => {
-            getContractsForSymbolFromApi(symbol).then(contractsForSymbolFromApi => {
-                resolve(getDurationsForContract(contractsForSymbolFromApi.available));
-            });
-        });
-
-    // Check if we have local data to get durations from
-    const contractsForSymbol = contractsForStore.find(c => c.symbol === symbol);
-    if (contractsForSymbol) {
-        const isDifferentAccount = () =>
-            tokenList.length && contractsForSymbol.accountName !== tokenList[0].accountName;
-        const isExpiredData = () => Math.floor((Date.now() - contractsForSymbol.timestamp) / 1000) > 600;
-        if (isDifferentAccount()) {
-            return getFreshContractsFor();
-        } else if (isExpiredData()) {
-            // Return cached data, update cached data in background
-            getContractsForSymbolFromApi(symbol);
-        }
-        return Promise.resolve(getDurationsForContract(contractsForSymbol.available));
+        globalObserver.emit('ui.log.error', translate('Could not retrieve contracts'));
     }
-    return getFreshContractsFor();
+    if (typeof api.disconnect === 'function') {
+        api.disconnect();
+    }
+    return contractsForSymbol;
+};
+
+export const getBarriersForContracts = (contracts, selectedContractType, selectedDuration) => {
+    const category = getContractCategory(selectedContractType);
+    const contractsForContractCategory = filterContractsByCategory(contracts, category, selectedContractType);
+    const barriers = {};
+    if (contractsForContractCategory) {
+        let contract;
+        if (selectedDuration) {
+            // Find barriers based on selected duration (e.g. Hours & days can have different barrier values)
+            contract = contractsForContractCategory.find(c => {
+                const durations = getDurationsForContracts([c], selectedContractType);
+                return durations.map(duration => duration.unit).includes(selectedDuration);
+            });
+        } else {
+            // Default to smallest barriers available
+            contract = contractsForContractCategory
+                .sort((a, b) => {
+                    const barrierValueA = a.barrier || a.high_barrier;
+                    const barrierValueB = b.barrier || b.high_barrier;
+                    return parseFloat(barrierValueA) - parseFloat(barrierValueB);
+                })
+                .shift();
+        }
+        if (contract) {
+            const offsetRegex = new RegExp('^[-|+]([0-9]+.[0-9]+)$');
+            if (contract.barriers === 2 && contract.high_barrier && contract.low_barrier) {
+                const highBarrierOffsetMatch = contract.high_barrier.toString().match(offsetRegex);
+                const lowBarrierOffsetMatch = contract.low_barrier.toString().match(offsetRegex);
+
+                if (highBarrierOffsetMatch && lowBarrierOffsetMatch) {
+                    barriers.high_barrier = highBarrierOffsetMatch[1]; // eslint-disable-line prefer-destructuring
+                    barriers.low_barrier = lowBarrierOffsetMatch[1]; // eslint-disable-line prefer-destructuring
+                } else {
+                    const countDecimals = number => {
+                        if (Math.floor(number) === number) return 0;
+                        return number.toString().split('.')[1].length || 0;
+                    };
+                    const highBarrier = parseFloat(contract.high_barrier);
+                    const lowBarrier = parseFloat(contract.low_barrier);
+                    const barrier = ((highBarrier - lowBarrier) / 2).toFixed(countDecimals(contract.high_barrier));
+                    barriers.high_barrier = barrier;
+                    barriers.low_barrier = barrier;
+                }
+            } else if (contract.barriers === 1 && contract.barrier) {
+                const barrierOffsetMatch = contract.barrier.toString().match(offsetRegex);
+                if (barrierOffsetMatch) {
+                    barriers.barrier = barrierOffsetMatch[1]; // eslint-disable-line prefer-destructuring
+                } else {
+                    // Return default value if we only get a barrier based on price (e.g. 234.1231)
+                    // as we can't calculate the offset since we don't stream price like SmartTrader
+                    barriers.barrier = 0.1;
+                }
+            }
+        }
+    }
+    return barriers;
+};
+
+export const getPredictionForContracts = (contracts, selectedContractType) => {
+    const category = getContractCategory(selectedContractType);
+    const contractsForContractCategory = filterContractsByCategory(contracts, category, selectedContractType);
+
+    const contractMapping = {};
+    if (category === 'digits') {
+        contractMapping.matchesdiffers = ['DIGITMATCH', 'DIGITDIFF'];
+        contractMapping.overunder = ['DIGITOVER', 'DIGITUNDER'];
+    } else if (category === 'highlowticks') {
+        contractMapping.highlowticks = ['TICKHIGH', 'TICKLOW'];
+    }
+
+    const predictionRange = [];
+    if (contractMapping[selectedContractType]) {
+        const contract = contractsForContractCategory.find(c =>
+            contractMapping[selectedContractType].includes(c.contract_type)
+        );
+        if (contract.last_digit_range) {
+            predictionRange.push(...contract.last_digit_range);
+        } else {
+            predictionRange.push(0);
+        }
+    }
+    return predictionRange;
 };
