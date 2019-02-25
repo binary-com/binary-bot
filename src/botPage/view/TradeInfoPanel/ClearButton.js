8408cf3d60b1e2000dd30f1f99da72572da17b28
Aaron Imming <aaron@binary.com>
2019-02-25 14:11:29 +0800

    Create ClearButton

---
+++ src/botPage/view/TradeInfoPanel/ClearButton.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { observer as globalObserver } from '../../../common/utils/observer';
+import { showDialog } from '../../bot/tools';
+import { translate } from '../../../common/utils/tools';
+
+const ClearButton = () => {
+    function confirmClearLog() {
+        showDialog({
+            title: translate('Are you sure?'),
+            text : [translate('This will clear all transactions in the summary panel.')],
+        })
+            .then(() => globalObserver.emit('summary.clear'))
+            .catch(() => {});
+    }
+    return (
+        <button
+            title="Clear summary log"
+            id="summaryClearButton"
+            className="toolbox-button icon-clear"
+            onClick={confirmClearLog}
+        />
+    );
+};
+
+export default ClearButton;
