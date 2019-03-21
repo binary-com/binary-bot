577b1c36f945277d623cdb7e7ffb69ecd59bb7dd
Aaron Imming <aaron@binary.com>
2019-03-21 14:19:06 +0800

    Make sure button is restored on cancel action

--- src/common/integrations/GoogleDrive.js
+++ src/common/integrations/GoogleDrive.js
@@ -93,40 +93,40 @@ class GoogleDrive {
         return new Promise((resolve, reject) => {
             // eslint-disable-next-line consistent-return
             const userPickedFile = data => {
-                if (data.action !== google.picker.Action.PICKED) {
-                    return reject(google.picker.Action.CANCEL);
-                }
-
-                const fileId = data.docs[0].id;
-                gapi.client.drive.files
-                    .get({
-                        alt     : 'media',
-                        fileId,
-                        mimeType: 'text/plain',
-                    })
-                    .then(response => {
-                        try {
-                            const xmlDom = Blockly.Xml.textToDom(response.body);
-                            const loadFunction =
-                                xmlDom.hasAttribute('collection') && xmlDom.getAttribute('collection') === 'true'
-                                    ? loadBlocks
-                                    : loadWorkspace;
+                if (data.action === google.picker.Action.PICKED) {
+                    const fileId = data.docs[0].id;
+                    gapi.client.drive.files
+                        .get({
+                            alt     : 'media',
+                            fileId,
+                            mimeType: 'text/plain',
+                        })
+                        .then(response => {
                             try {
-                                loadFunction(xmlDom);
-                                resolve();
+                                const xmlDom = Blockly.Xml.textToDom(response.body);
+                                const loadFunction =
+                                    xmlDom.hasAttribute('collection') && xmlDom.getAttribute('collection') === 'true'
+                                        ? loadBlocks
+                                        : loadWorkspace;
+                                try {
+                                    loadFunction(xmlDom);
+                                    resolve();
+                                } catch (error) {
+                                    trackAndEmitError(translate('Could not load Google Drive blocks'), error);
+                                    reject(error);
+                                }
                             } catch (error) {
-                                trackAndEmitError(translate('Could not load Google Drive blocks'), error);
+                                trackAndEmitError(translate('Unrecognized file format'), error);
                                 reject(error);
                             }
-                        } catch (error) {
-                            trackAndEmitError(translate('Unrecognized file format'), error);
+                        })
+                        .catch(error => {
+                            trackAndEmitError(translate('There was an error retrieving data from Google Drive'), error);
                             reject(error);
-                        }
-                    })
-                    .catch(error => {
-                        trackAndEmitError(translate('There was an error retrieving data from Google Drive'), error);
-                        reject(error);
-                    });
+                        });
+                } else if (data.action === google.picker.Action.CANCEL) {
+                    reject();
+                }
             };
 
             this.authorise()
@@ -139,6 +139,7 @@ class GoogleDrive {
 
                     const picker = new google.picker.PickerBuilder();
                     picker
+                        .setOrigin(`${window.location.protocol}//${window.location.host}`)
                         .setTitle(translate('Select a Binary Bot strategy'))
                         .setLocale(this.getPickerLanguage())
                         .setAppId(this.appId)
@@ -149,7 +150,7 @@ class GoogleDrive {
                         .build()
                         .setVisible(true);
                 })
-                .catch(() => reject());
+                .catch(error => reject(error));
         });
     }
 
@@ -202,35 +203,35 @@ class GoogleDrive {
         return new Promise((resolve, reject) => {
             // eslint-disable-next-line consistent-return
             const savePickerCallback = data => {
-                if (data.action !== google.picker.Action.PICKED) {
-                    return reject(google.picker.Action.CANCEL);
+                if (data.action === google.picker.Action.PICKED) {
+                    const folderId = data.docs[0].id;
+                    const strategyFile = new Blob([options.content], { type: options.mimeType });
+                    const strategyFileMetadata = JSON.stringify({
+                        name    : options.name,
+                        mimeType: options.mimeType,
+                        parents : [folderId],
+                    });
+
+                    const formData = new FormData();
+                    formData.append('metadata', new Blob([strategyFileMetadata], { type: 'application/json' }));
+                    formData.append('file', strategyFile);
+
+                    const xhr = new XMLHttpRequest();
+                    xhr.responseType = 'json';
+                    xhr.open('POST', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart');
+                    xhr.setRequestHeader('Authorization', `Bearer ${gapi.auth.getToken().access_token}`);
+                    xhr.onload = () => {
+                        if (xhr.status === 200) {
+                            resolve();
+                        } else {
+                            trackAndEmitError(translate('There was an error processing your request'), xhr.status);
+                            reject();
+                        }
+                    };
+                    xhr.send(formData);
+                } else if (data.action === google.picker.Action.CANCEL) {
+                    reject();
                 }
-
-                const folderId = data.docs[0].id;
-                const strategyFile = new Blob([options.content], { type: options.mimeType });
-                const strategyFileMetadata = JSON.stringify({
-                    name    : options.name,
-                    mimeType: options.mimeType,
-                    parents : [folderId],
-                });
-
-                const formData = new FormData();
-                formData.append('metadata', new Blob([strategyFileMetadata], { type: 'application/json' }));
-                formData.append('file', strategyFile);
-
-                const xhr = new XMLHttpRequest();
-                xhr.responseType = 'json';
-                xhr.open('POST', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart');
-                xhr.setRequestHeader('Authorization', `Bearer ${gapi.auth.getToken().access_token}`);
-                xhr.onload = () => {
-                    if (xhr.status === 200) {
-                        resolve(xhr.response);
-                    } else {
-                        trackAndEmitError(translate('There was an error processing your request'), xhr.status);
-                        reject();
-                    }
-                };
-                xhr.send(formData);
             };
 
             this.authorise()
@@ -246,6 +247,7 @@ class GoogleDrive {
 
                         const picker = new google.picker.PickerBuilder();
                         picker
+                            .setOrigin(`${window.location.protocol}//${window.location.host}`)
                             .setTitle(translate('Select a folder'))
                             .addView(view)
                             .setLocale(this.getPickerLanguage())
@@ -257,7 +259,7 @@ class GoogleDrive {
                             .setVisible(true);
                     });
                 })
-                .catch(() => reject());
+                .catch(error => reject(error));
         });
     }
 }
