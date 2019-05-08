e42ac5512cc91c3f962b461df09eae9b347433b9
Aaron Imming <aaron@binary.com>
2019-05-08 17:06:59 +0800

    Only render everything after 'website_status'

--- src/indexPage/index.js
+++ src/indexPage/index.js
@@ -1,40 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import endpoint from './endpoint';
 import Logo from './react-components/logo.jsx';
 import Footer from './react-components/footer.jsx';
-import { getTokenList } from '../common/utils/storageManager';
-import { oauthLogin } from '../common/appId';
-import { createUrl } from '../common/utils/tools';
-import { load as loadLang } from '../common/lang';
+import { oauthLogin, generateLiveApiInstance } from '../common/appId';
 import '../common/binary-ui/dropdown';
-import endpoint from './endpoint';
-import isEuCountry from '../common/footer-checks';
+import { load as loadLang } from '../common/lang';
+import { getTokenList } from '../common/utils/storageManager';
+import { createUrl } from '../common/utils/tools';
 
-const renderElements = () => {
-    const showHideEuElements = isEu => {
-        $('.eu-hide').attr('style', `display: ${isEu ? 'none' : 'block'} !important`);
-        $('.eu-show, .eu-only').attr('style', `display: ${isEu ? 'block' : 'none'} !important`);
-    };
-    ReactDOM.render(<Logo />, document.getElementById('binary-logo'));
-    ReactDOM.render(<Footer />, document.getElementById('footer'));
-    isEuCountry().then(isEu => {
-        showHideEuElements(isEu);
-    });
-    $('#shop-url').attr('href', createUrl({ subdomain: 'shop', path: 'collections/strategies', isNonBotPage: true }));
-};
+const init = async () => {
+    if (endpoint()) {
+        return;
+    }
 
-const loginCheck = () => {
-    if (endpoint()) return;
     if (getTokenList().length) {
         window.location.pathname = `${window.location.pathname.replace(/\/+$/, '')}/bot.html`;
-    } else {
-        loadLang();
-        oauthLogin(() => {
-            $('.show-on-load').show();
-            $('.barspinner').hide();
-            renderElements();
-        });
+        return;
     }
+
+    const api = generateLiveApiInstance();
+
+    let isEurope = false;
+    let isIndonesia = false;
+
+    try {
+        const {
+            website_status: { clients_country: clientsCountry },
+        } = await api.getWebsiteStatus();
+
+        if (clientsCountry === 'id') {
+            isIndonesia = true;
+        } else {
+            const {
+                landing_company: { financial_company: financialCompany, gaming_company: gamingCompany },
+            } = await api.getLandingCompany({ landing_company: clientsCountry });
+
+            const financialShortcode = financialCompany ? financialCompany.shortcode : false;
+            const gamingShortcode = gamingCompany ? gamingCompany.shortcode : false;
+
+            if (financialShortcode || gamingShortcode) {
+                const euShortcodeRegex = new RegExp('^(maltainvest|malta|iom)$');
+                isEurope = euShortcodeRegex.test(financialShortcode) || euShortcodeRegex.test(gamingShortcode);
+            } else {
+                isEurope = /^mt$/.test(clientsCountry);
+            }
+        }
+    } catch (e) {
+        // Do nothing.
+    }
+
+    loadLang();
+    oauthLogin(() => {
+        $('.show-on-load').show();
+        $('.barspinner').hide();
+        renderElements(isEurope, isIndonesia);
+    });
 };
 
-loginCheck();
+const renderElements = (isEurope, isIndonesia) => {
+    ReactDOM.render(<Logo />, document.getElementById('binary-logo'));
+    ReactDOM.render(<Footer isIndonesia={isIndonesia} />, document.getElementById('footer'));
+
+    $('.eu-hide').attr('style', `display: ${isEurope ? 'none' : 'block'} !important`);
+    $('.eu-show, .eu-only').attr('style', `display: ${isEurope ? 'block' : 'none'} !important`);
+
+    $('#shop-url').attr(
+        'href',
+        createUrl({
+            subdomain   : 'shop',
+            path        : 'collections/strategies',
+            isNonBotPage: true,
+        })
+    );
+};
+
+init();
