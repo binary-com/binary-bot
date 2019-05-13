3c4c865617952386c0161bef3f60daf351ec4be7
Aaron Imming <aaron@binary.com>
2019-05-13 17:12:03 +0800

    Block definition for notify_telegram type block

---
+++ src/botPage/view/blockly/blocks/tools/notify_telegram.js
@@ -0,0 +1,44 @@
+import { translate } from '../../../../../common/i18n';
+
+Blockly.Blocks.notify_telegram = {
+    init() {
+        this.jsonInit({
+            message0: translate('Notify Telegram %1 Bot ID: %2 Chat ID: %3 Message: %4'),
+            args0   : [
+                {
+                    type: 'input_dummy',
+                },
+                {
+                    type: 'input_value',
+                    name: 'TELEGRAM_BOT_ID',
+                },
+                {
+                    type: 'input_value',
+                    name: 'TELEGRAM_CHAT_ID',
+                },
+                {
+                    type: 'input_value',
+                    name: 'TELEGRAM_MESSAGE',
+                },
+            ],
+            colour           : '#dedede',
+            inputsInline     : true,
+            previousStatement: null,
+            nextStatement    : null,
+            tooltip          : translate('Sends a message to Telegram'),
+        });
+    },
+};
+
+Blockly.JavaScript.notify_telegram = block => {
+    const botId = Blockly.JavaScript.valueToCode(block, 'TELEGRAM_BOT_ID') || '';
+    const chatId = Blockly.JavaScript.valueToCode(block, 'TELEGRAM_CHAT_ID') || '';
+    const message = Blockly.JavaScript.valueToCode(block, 'TELEGRAM_MESSAGE') || '';
+
+    if (!botId || !chatId || !message) {
+        return '';
+    }
+
+    const code = `Bot.notifyTelegram(${botId}, ${chatId}, ${message});\n`;
+    return code;
+};
