2a12d94fedb8bb26e5f54d8719b74e0cf28d896d
Aaron Imming <aaron@binary.com>
2019-05-13 13:11:48 +0800

    Check if dataLayer is available, init instead of setVisitorId

--- src/common/gtm.js
+++ src/common/gtm.js
@@ -7,7 +7,8 @@ const GTM = (() => {
 
     const init = () => {
         if (isGtmApplicable()) {
-            const gtmTag = '(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\': new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src= \'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f); })(window,document,\'script\',\'dataLayer\',\'GTM-P97C2DZ\');';
+            const gtmTag =
+                '(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\': new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src= \'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f); })(window,document,\'script\',\'dataLayer\',\'GTM-P97C2DZ\');';
 
             const script = document.createElement('script');
             script.innerHTML = gtmTag;
@@ -23,7 +24,7 @@ const GTM = (() => {
     };
 
     const pushDataLayer = data => {
-        if (isGtmApplicable()) {
+        if (isGtmApplicable() && dataLayer) {
             dataLayer.push({
                 ...data,
             });
