2719b92288d828ef5385c1bd9afd3a0a375de85a
Aaron Imming <aaron@binary.com>
2019-05-13 17:10:45 +0800

    Create notifyTelegram logic

--- src/botPage/bot/Interface/MiscInterface.js
+++ src/botPage/bot/Interface/MiscInterface.js
@@ -1,10 +1,26 @@
+import { notify } from '../broadcast';
+import { translate } from '../../../common/i18n';
 import { observer as globalObserver } from '../../../common/utils/observer';
 
 export default Interface =>
     class extends Interface {
+        notifyTelegram(botId, chatId, message) {
+            const url = encodeURI(`https://api.telegram.org/bot${botId}/sendMessage?chat_id=${chatId}&text=${message}`);
+            const onError = () => notify('warn', translate('The Telegram notification could not be sent'));
+
+            fetch(url)
+                .then(response => {
+                    if (!response.ok) {
+                        onError();
+                    }
+                })
+                .catch(onError);
+        }
+
         getMiscInterface() {
             return {
                 notify        : args => globalObserver.emit('Notify', args),
+                notifyTelegram: this.notifyTelegram,
                 getTotalRuns  : () => this.tradeEngine.getTotalRuns(),
                 getBalance    : type => this.tradeEngine.getBalance(type),
                 getTotalProfit: () => this.tradeEngine.getTotalProfit(),
