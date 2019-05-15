1e60dbecdd66d98d965b85e2d91330541c75641f
Aaron Imming <aaron@binary.com>
2019-05-15 14:55:27 +0800

    Create math_number_positive block

--- src/botPage/view/blockly/blocks/Scratch/Math/index.js
+++ src/botPage/view/blockly/blocks/Scratch/Math/index.js
@@ -3,6 +3,7 @@ import './math_constant';
 import './math_constrain';
 import './math_modulo';
 import './math_number';
+import './math_number_positive';
 import './math_number_property';
 import './math_on_list';
 import './math_random_float';
@@ -11,4 +12,3 @@ import './math_round';
 import './math_arithmetic';
 import './math_single';
 import './math_trig';
-
---
+++ src/botPage/view/blockly/blocks/Scratch/Math/math_number_positive.js
@@ -0,0 +1,14 @@
+Blockly.Blocks.math_number_positive = {
+    init: Blockly.Blocks.math_number.init,
+    numberValidator(input) {
+        if (/^[0-9][0-9,\.]*$/.test(input)) {
+            return undefined;
+        }
+        return null;
+    },
+};
+
+Blockly.JavaScript.math_number_positive = block => {
+    const code = block.getFieldValue('NUM');
+    return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
