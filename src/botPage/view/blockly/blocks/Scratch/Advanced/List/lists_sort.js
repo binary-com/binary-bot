ece01569883d7dcbbf8eaafabd5d75cabf343949
Aaron Imming <aaron@binary.com>
2019-06-10 16:58:43 +0800

    Simplify sorting by removing ignore_case option

--- src/botPage/view/blockly/blocks/Scratch/Advanced/List/lists_sort.js
+++ src/botPage/view/blockly/blocks/Scratch/Advanced/List/lists_sort.js
@@ -8,16 +8,12 @@ Blockly.Blocks.lists_sort = {
                 {
                     type   : 'field_dropdown',
                     name   : 'TYPE',
-                    options: [
-                        [translate('numeric'), 'NUMERIC'],
-                        [translate('alphabetic'), 'TEXT'],
-                        [translate('alphabetic, ignore case'), 'IGNORE_CASE'],
-                    ],
+                    options: [[translate('numeric'), 'NUMERIC'], [translate('alphabetic'), 'TEXT']],
                 },
                 {
                     type   : 'field_dropdown',
                     name   : 'DIRECTION',
-                    options: [[translate('ascending'), 'ASCENDING'], [translate('descending'), 'DESCENDING']],
+                    options: [[translate('ascending'), '1'], [translate('descending'), '-1']],
                 },
                 {
                     type: 'input_value',
@@ -35,36 +31,27 @@ Blockly.Blocks.lists_sort = {
 
 Blockly.JavaScript.lists_sort = block => {
     const list = Blockly.JavaScript.valueToCode(block, 'LIST', Blockly.JavaScript.ORDER_FUNCTION_CALL) || '[]';
-    const direction = block.getFieldValue('DIRECTION') === 'ASCENDING' ? 1 : -1;
+    const direction = block.getFieldValue('DIRECTION') === '1' ? 1 : -1;
     const type = block.getFieldValue('TYPE');
     // eslint-disable-next-line no-underscore-dangle
     const getCompareFunctionName = Blockly.JavaScript.provideFunction_('listsGetSortCompare', [
         // eslint-disable-next-line no-underscore-dangle
         `function ${Blockly.JavaScript.FUNCTION_NAME_PLACEHOLDER_}(type, direction) {
             var compareFuncs = {
-                "NUMERIC": function(a, b) {
+                NUMERIC: function(a, b) {
                     return parseFloat(a) - parseFloat(b);
                 },
-                "TEXT": function(a, b) {
-                    var a_ = a.toString();
-                    var b_ = b.toString();
-                    return (a_ === b_ ? 0 : a_ > b_ ? 1 : -1);
-                },
-                "IGNORE_CASE": function(a, b) {
-                    var a_ = a.toString().toLowerCase();
-                    var b_ = b.toString().toLowerCase();
-                    return (a_ === b_ ? 0 : a_ > b_ ? 1 : -1);
+                TEXT: function(a, b) {
+                    return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1;
                 }
             };
 
-            var compare = compareFuncs[type];
-            
-            return function(a, b) { 
-                return compare(a, b) * direction; 
+            return function(a, b) {
+                return compareFuncs[type](a, b) * direction; 
             }
         }`,
     ]);
 
-    const code = `${list}.slice().sort(${getCompareFunctionName}('${type}', ${direction}))`;
+    const code = `${list}.slice(0).sort(${getCompareFunctionName}("${type}", ${direction}))`;
     return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
 };
