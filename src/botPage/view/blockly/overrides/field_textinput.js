1d62e0e9474201f9897e85002b4084eb166b7f43
Aaron Imming <aaron@binary.com>
2019-06-10 17:06:30 +0800

    Fix eslinting errors

--- src/botPage/view/blockly/overrides/field_textinput.js
+++ src/botPage/view/blockly/overrides/field_textinput.js
@@ -1,23 +1,20 @@
+/* eslint-disable no-underscore-dangle, func-names */
+
 /**
  * Show the inline free-text editor on top of the text.
- * @param {boolean=} opt_quietInput True if editor should be created without
+ * @param {boolean=} optQuietInput True if editor should be created without
  *     focus.  Defaults to false.
- * @param {boolean=} opt_readOnly True if editor should be created with HTML
+ * @param {boolean=} optReadOnly True if editor should be created with HTML
  *     input set to read-only, to prevent virtual keyboards.
- * @param {boolean=} opt_withArrow True to show drop-down arrow in text editor.
- * @param {Function=} opt_arrowCallback Callback for when drop-down arrow clicked.
+ * @param {boolean=} optWithArrow True to show drop-down arrow in text editor.
+ * @param {Function=} optArrowCallback Callback for when drop-down arrow clicked.
  * @private
  */
-Blockly.FieldTextInput.prototype.showEditor_ = function(
-    opt_quietInput,
-    opt_readOnly,
-    opt_withArrow,
-    opt_arrowCallback
-) {
+Blockly.FieldTextInput.prototype.showEditor_ = function(optQuietInput, optReadOnly, optWithArrow, optArrowCallback) {
     this.workspace_ = this.sourceBlock_.workspace;
 
-    const quietInput = opt_quietInput || false;
-    const readOnly = opt_readOnly || false;
+    const quietInput = optQuietInput || false;
+    const readOnly = optReadOnly || false;
 
     Blockly.WidgetDiv.show(
         this,
@@ -46,19 +43,19 @@ Blockly.FieldTextInput.prototype.showEditor_ = function(
 
     div.appendChild(htmlInput);
 
-    if (opt_withArrow) {
+    if (optWithArrow) {
         // Move text in input to account for displayed drop-down arrow.
         if (this.sourceBlock_.RTL) {
-            htmlInput.style.paddingLeft = `${this.arrowSize_ + Blockly.BlockSvg.DROPDOWN_ARROW_PADDING  }px`;
+            htmlInput.style.paddingLeft = `${this.arrowSize_ + Blockly.BlockSvg.DROPDOWN_ARROW_PADDING}px`;
         } else {
-            htmlInput.style.paddingRight = `${this.arrowSize_ + Blockly.BlockSvg.DROPDOWN_ARROW_PADDING  }px`;
+            htmlInput.style.paddingRight = `${this.arrowSize_ + Blockly.BlockSvg.DROPDOWN_ARROW_PADDING}px`;
         }
         // Create the arrow.
         const dropDownArrow = goog.dom.createDom(goog.dom.TagName.IMG, 'blocklyTextDropDownArrow');
-        dropDownArrow.setAttribute('src', `${Blockly.mainWorkspace.options.pathToMedia  }dropdown-arrow-dark.svg`);
-        dropDownArrow.style.width = `${this.arrowSize_  }px`;
-        dropDownArrow.style.height = `${this.arrowSize_  }px`;
-        dropDownArrow.style.top = `${this.arrowY_  }px`;
+        dropDownArrow.setAttribute('src', `${Blockly.mainWorkspace.options.pathToMedia}dropdown-arrow-dark.svg`);
+        dropDownArrow.style.width = `${this.arrowSize_}px`;
+        dropDownArrow.style.height = `${this.arrowSize_}px`;
+        dropDownArrow.style.top = `${this.arrowY_}px`;
         dropDownArrow.style.cursor = 'pointer';
 
         // Magic number for positioning the drop-down arrow on top of the text editor.
@@ -70,19 +67,19 @@ Blockly.FieldTextInput.prototype.showEditor_ = function(
             dropDownArrow.style.right = dropdownArrowMagic;
         }
 
-        if (opt_arrowCallback) {
+        if (optArrowCallback) {
             htmlInput.dropDownArrowMouseWrapper_ = Blockly.bindEvent_(
                 dropDownArrow,
                 'mousedown',
                 this,
-                opt_arrowCallback
+                optArrowCallback
             );
         }
 
         div.appendChild(dropDownArrow);
     }
 
-    htmlInput.value = htmlInput.defaultValue = this.text_;
+    htmlInput.value = htmlInput.defaultValue = this.text_; // eslint-disable-line no-multi-assign
     htmlInput.oldValue_ = null;
 
     this.validate_();
@@ -98,29 +95,21 @@ Blockly.FieldTextInput.prototype.showEditor_ = function(
     this.bindEvents_(htmlInput);
 
     // Add animation transition properties
-    const transitionProperties = `box-shadow ${  Blockly.FieldTextInput.ANIMATION_TIME  }s`;
+    const transitionProperties = `box-shadow ${Blockly.FieldTextInput.ANIMATION_TIME}s`;
 
     if (Blockly.BlockSvg.FIELD_TEXTINPUT_ANIMATE_POSITIONING) {
         div.style.transition +=
-            `,padding ${ 
-                Blockly.FieldTextInput.ANIMATION_TIME 
-            }s,` +
-            `width ${ 
-                Blockly.FieldTextInput.ANIMATION_TIME 
-            }s,` +
-            `height ${ 
-                Blockly.FieldTextInput.ANIMATION_TIME 
-            }s,` +
-            `margin-left ${ 
-                Blockly.FieldTextInput.ANIMATION_TIME 
-            }s`;
+            `,padding ${Blockly.FieldTextInput.ANIMATION_TIME}s,` +
+            `width ${Blockly.FieldTextInput.ANIMATION_TIME}s,` +
+            `height ${Blockly.FieldTextInput.ANIMATION_TIME}s,` +
+            `margin-left ${Blockly.FieldTextInput.ANIMATION_TIME}s`;
     }
 
     div.style.transition = transitionProperties;
-    htmlInput.style.transition = `font-size ${  Blockly.FieldTextInput.ANIMATION_TIME  }s`;
+    htmlInput.style.transition = `font-size ${Blockly.FieldTextInput.ANIMATION_TIME}s`;
 
     // The animated properties themselves
-    htmlInput.style.fontSize = `${Blockly.BlockSvg.FIELD_TEXTINPUT_FONTSIZE_FINAL  }pt`;
+    htmlInput.style.fontSize = `${Blockly.BlockSvg.FIELD_TEXTINPUT_FONTSIZE_FINAL}pt`;
 
-    div.style.boxShadow = `0px 0px 0px 4px ${  Blockly.Colours.fieldShadow}`;
+    div.style.boxShadow = `0px 0px 0px 4px ${Blockly.Colours.fieldShadow}`;
 };
