1bbaaff8f1ef08197ec3e2253dc14a11313be358
Aaron Imming <aaim@protonmail.com>
2020-02-17 17:02:13 +0800

    Minor refactor

--- src/botPage/bot/TradeEngine/Proposal.js
+++ src/botPage/bot/TradeEngine/Proposal.js
@@ -55,31 +55,30 @@ export default Engine =>
             Promise.all(
                 this.proposalTemplates.map(proposal =>
                     doUntilDone(() =>
-                        this.api
-                            .subscribeToPriceForContractProposal(proposal)
-                            // eslint-disable-next-line consistent-return
-                            .catch(e => {
-                                if (e && e.name === 'RateLimit') {
-                                    return Promise.reject(e);
+                        this.api.subscribeToPriceForContractProposal(proposal).catch(e => {
+                            if (e && e.name === 'RateLimit') {
+                                return e;
+                            }
+
+                            const errorCode = e.error && e.error.error && e.error.error.code;
+
+                            if (errorCode === 'ContractBuyValidationError') {
+                                const { uuid } = e.error.echo_req.passthrough;
+
+                                if (!this.data.hasIn(['forgetProposals', uuid])) {
+                                    // Add to proposals map with error. Will later be shown to user, see selectProposal.
+                                    this.data = this.data.setIn(['proposals', uuid], {
+                                        ...proposal,
+                                        ...proposal.passthrough,
+                                        error: e,
+                                    });
                                 }
 
-                                const errorCode = e.error && e.error.error && e.error.error.code;
+                                return null;
+                            }
 
-                                if (errorCode === 'ContractBuyValidationError') {
-                                    const { uuid } = e.error.echo_req.passthrough;
-
-                                    if (!this.data.hasIn(['forgetProposals', uuid])) {
-                                        // Add to proposals map with error. Will later be shown to user, see selectProposal.
-                                        this.data = this.data.setIn(['proposals', uuid], {
-                                            ...proposal,
-                                            contractType: proposal.contract_type,
-                                            error       : e,
-                                        });
-                                    }
-                                } else {
-                                    this.$scope.observer.emit('Error', e);
-                                }
-                            })
+                            return e;
+                        })
                     )
                 )
             ).catch(e => this.$scope.observer.emit('Error', e));
