484449c7ecb15457973fdea27835c89882255328
Henadzi Burkhanau <henadzi@deriv.com>
2021-08-25 11:33:44 +0300

    removed shop urls

--- src/botPage/view/tour/welcome.js
+++ src/botPage/view/tour/welcome.js
@@ -37,9 +37,6 @@ const steps = [
         title: translate('Workspace'),
         text : `<p>
       ${translate('Drag and drop block files.')}
-      <a target="blank" href="https://shop.binary.com/collections/strategies">
-        ${translate('Download sample strategies')}
-      </a>
       ${translate('or')} ${translate('make your own strategies.')}
     </p>`,
         selector: '#workspace_center',
