fdf52fb18f053270079ce547c92468564535b95b
sandeep <sandeep@deriv.com>
2022-01-10 17:26:58 +0800

    Updated variable names

--- src/botPage/view/deriv/components/SidebarToggle/index.js
+++ src/botPage/view/deriv/components/SidebarToggle/index.js
@@ -1,33 +1,38 @@
 import React from 'react';
-import cn from 'classnames';
+import classNames from 'classnames';
 import { isMobile } from '../../../../../common/utils/tools';
 
 const SidebarToggle = () => {
-    const toggleRef = React.useRef();
-    const [open, setOpen] = React.useState(false);
+    const toggle_ref = React.useRef();
+    const [is_open, setOpen] = React.useState(false);
 
     React.useEffect(() => {
         if (isMobile()) {
             const blockly_sidebar_container = document.getElementsByClassName('injectionDiv')?.[0];
-            if (blockly_sidebar_container && toggleRef.current) {
-                blockly_sidebar_container.appendChild(toggleRef.current);
+            if (blockly_sidebar_container && toggle_ref.current) {
+                blockly_sidebar_container.appendChild(toggle_ref.current);
             }
         }
     }, []);
 
     React.useEffect(() => {
-        const blocklyToolbox = document.getElementsByClassName('blocklyToolboxDiv')?.[0];
-        if (blocklyToolbox) {
-            if (!open) {
-                blocklyToolbox.classList.remove('open');
+        const blockly_toolbox = document.getElementsByClassName('blocklyToolboxDiv')?.[0];
+        if (blockly_toolbox) {
+            if (!is_open) {
+                blockly_toolbox.classList.remove('open');
             } else {
-                blocklyToolbox.classList.add('open');
+                blockly_toolbox.classList.add('open');
             }
         }
-    }, [open]);
+    }, [is_open]);
 
     return (
-        <div id="sidebar-toggle-action" className={cn({ open })} ref={toggleRef} onClick={() => setOpen(!open)}>
+        <div
+            id="sidebar-toggle-action"
+            className={classNames({ open: is_open })}
+            ref={toggle_ref}
+            onClick={() => setOpen(!is_open)}
+        >
             <span>
                 <img src="image/deriv/ic-chevron-down-bold.svg" />
             </span>
