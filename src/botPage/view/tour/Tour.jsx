b5a74f025ddfcabff6a0ba7b7feb1cb7c0338f75
sara-fs <sara.khosropour@firstsource.tech>
2022-01-18 11:31:05 +0330

    Refactored tour component part 2

--- src/botPage/view/tour/Tour.jsx
+++ src/botPage/view/tour/Tour.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Joyride, { STATUS, ACTIONS, EVENTS } from 'react-joyride';
+
 import {
     set as setStorage,
     get as getStorage,
@@ -8,32 +9,24 @@ import { translate } from '../../../common/i18n';
 import welcome from './welcome.jsx';
 import { isMobile } from '../../../common/utils/tools';
 
-
 const Tour = () => {
     const[run, setRun] = useState(!getStorage('closedTourPopup'));
     const[stepIndex, setStepIndex] = useState(0);
 
     const closeTourPermanently = () => {
-        // e.preventDefault();
         setStorage('closedTourPopup', Date.now());
         setRun(false);
     };
     const continueTour = (is_checked) => {
-        // e.preventDefault();
-        console.log('is_checked.',is_checked);
         if(is_checked){
             setStorage('closedTourPopup', Date.now());
         }
         setStepIndex(stepIndex + 1);
-        console.log('stepindex', stepIndex);
     };
-
     const steps = welcome(closeTourPermanently, continueTour);
 
     const callback = (data) => {
-        console.log('dataaaa:', data);
         const { action, index, status , type} = data;
-
         if ([EVENTS.STEP_AFTER, EVENTS.TARGET_NOT_FOUND].includes(type)) {
             setStepIndex(index + (action === ACTIONS.PREV ? -1 : 1));
         }
@@ -49,12 +42,34 @@ const Tour = () => {
     return (
         <div className='tour-first-pop-up'>
             <Joyride
+                beaconComponent={undefined}
+                // beaconComponent={
+                //     React.forwardRef((props, ref) => (
+                //         <div onClick={()=> console.log('lskdlaskjd')} ref={ref} {...props} 
+                //          style={{
+                //             backgroundColor: '#fff',
+                //             borderRadius: '4px',
+                //             color: '#555',
+                //             cursor: 'default',
+                //             filter: 'drop-shadow(-1px -2px 3px rgba(0,0,0,0.3)) drop-shadow(1px 2px 3px rgba(0,0,0,0.3))',
+                //             padding: '20px',
+                //             pointerEvents: 'auto',
+                //             transform: 'translate3d(0,0,0)',
+                //             width: '17em',
+                //             zIndex: '1510',
+                //         }}>
+                //             <CustomBeaconComponent />
+                //         </div>
+                //     ))
+                // }
+               
                 run={run}
                 disableCloseOnEsc={true}
                 disableOverlay={!isMobile()}
                 disableOverlayClicks={true}
                 continuous={true}
                 locale={{
+                    open: '',
                     last: translate('Done'),
                 }}
                 stepIndex={stepIndex}
@@ -79,7 +94,6 @@ const Tour = () => {
                         color: '#fff',
                         padding: '6px 12px',
                         transition: 'background-color 0.2s ease-in-out',
-
                     // &:active,
                     // &:focus,
                     // &:hover {
@@ -96,6 +110,7 @@ const Tour = () => {
                         margin: '0',
                         borderBottom: '1px solid #e98024',
                     },
+                    
                 }}
             />
         </div>
--- src/botPage/view/tour/welcome.jsx
+++ src/botPage/view/tour/welcome.jsx
@@ -3,85 +3,108 @@ import { translate } from '../../../common/i18n';
 import { isLoggedInDeriv } from '../../../common/utils/storageManager';
 import { isMobile } from '../../../common/utils/tools';
 
-const FirstStep = ({closeTourPermanently, continueTour}) => (
-    <div>
-        <p>
-            {translate('Ready to learn how to use Binary Bot?')}
-        </p>  
+
+const CustomBeaconComponent = ({closeTourPermanently, continueTour}) => (
+    <div style={{
+        backgroundColor: '#fff',
+        borderRadius: '4px',
+        color: '#555',
+        cursor: 'default',
+        filter: 'drop-shadow(-1px -2px 3px rgba(0,0,0,0.3)) drop-shadow(1px 2px 3px rgba(0,0,0,0.3))',
+        padding: '20px',
+        pointerEvents: 'auto',
+        transform: 'translate3d(0,0,0)',
+        width: '17em',
+        zIndex: '1510',
+    }}>
         <div>
-            <a class="button-secondary" onClick={closeTourPermanently}><span>{translate('No Thanks')}</span></a>
-            <a class="button" onClick={()=> continueTour(document.getElementById('do-not-ask-me-again').checked)}><span>{translate('Yes')}</span></a>
-        </div>
-        <div class="tour-custom-buttons">
-            <input type="checkbox" id="do-not-ask-me-again"></input>
-            <label for="do-not-ask-me-again">
-                {translate('Do not ask me again.')}
-            </label>
+            <p>
+                {translate('Ready to learn how to use Binary Bot?')}
+            </p>  
+            <div>
+                <a class="button-secondary" onClick={closeTourPermanently}><span>{translate('No Thanks')}</span></a>
+                <a class="button" onClick={()=> continueTour(document.getElementById('chkAskAgain').checked)}>
+                    <span>{translate('Yes')}</span></a>
+            </div>
+            <div className="tour-custom-buttons">
+                <input type="checkbox" id="chkAskAgain" />
+                <label for="do-not-ask-me-again">
+                    {translate('Do not ask me again.')}
+                </label>
+            </div>
         </div>
     </div>
-    
 );
 
-function welcome(closeTourPermanently, continueTour) {
+
+const SecondStep = () => (
+    <p>{translate('Drag and drop block files or make your own strategies.')}</p>
+);
+
+const ThirdStep = () => (
+    <p>{translate('Add more blocks from here to your bot.')}</p>
+);
+
+const ForthStep = () => (
+    <p>
+        {translate('Login before starting your bot. Always test your strategies with the virtual account.')}
+    </p>
+);
+
+const FifthStep = () => (
+    <p>
+        {translate('Control your blocks. Hold the cursor on each button for more info.')}
+    </p>
+);
+
+const SixthStep = () => (
+    <p>
+        {translate('Want to report an issue or ask for help?')}
+        <a target="blank" href="https://github.com/binary-com/binary-bot/issues/new">
+            {translate('Click here')}
+        </a>
+    </p>
+)
+
+
+function welcome (closeTourPermanently, continueTour) {
+
     const steps = [
         {
-            title: translate('Take a quick tour'),
-            content: <FirstStep closeTourPermanently={closeTourPermanently} continueTour={continueTour}/>,
-            target: '#center',
-            position: 'center',
-            isFixed: true,
-            offset: 0,
-            disableBeacon: false,
-            style: {
-                contentAlign: 'center',
-                arrow: {
-                    display: 'none',
-                },
-                button: {
-                    display: 'none',
-                },
-                close: {
-                    display: 'none',
-                },
-            },
+            title: 'Take a quick tour',
+            content: <CustomBeaconComponent closeTourPermanently={closeTourPermanently}  continueTour={continueTour}  />,
+            target: '#workspace_center',
+            offset: 200,
+            disableBeacon: true,
         },
         {
             title: translate('Workspace'),
-            content: `<p>${translate('Drag and drop block files or make your own strategies.')}</p>`,
+            content: <SecondStep />,
             target: isMobile() ? '.injectionDiv' : '#workspace_center',
             position: isMobile() ? 'center' : 'top',
             offset: 200,
         },
         {
             title: translate('Blocks toolbox'),
-            content: `<p>${translate('Add more blocks from here to your bot.')}</p>`,
+            content: <ThirdStep />,
             target: '.blocklyTreeRoot',
             position: 'right',
         },
         {
             title: translate('Accounts'),
-            content: `<p>
-          ${translate('Login before starting your bot. Always test your strategies with the virtual account.')}
-        </p>`,
+            content: <ForthStep />,
             target: isLoggedInDeriv() ? '#acc_switcher' : '.header__btn',
             position: 'left',
         },
         {
             title: translate('Bot controls'),
-            content: `<p>
-          ${translate('Control your blocks. Hold the cursor on each button for more info.')}
-        </p>`,
+            content: <FifthStep />,
             target: isMobile() ? '#toolbox' : '#zoomIn',
             position: 'left',
         },
         {
             title: translate('Enjoy!'),
-            content: `<p>
-          ${translate('Want to report an issue or ask for help?')}
-          <a target="blank" href="https://github.com/binary-com/binary-bot/issues/new">
-            ${translate('Click here')}
-          </a>
-        </p>`,
+            content: <SixthStep />,
             target: '#center',
             position: 'center',
             style: {
@@ -93,6 +116,7 @@ function welcome(closeTourPermanently, continueTour) {
             offset: 0,
         },
     ];
+
     return steps;
 }
 
