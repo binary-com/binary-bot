aad7ede70efb12b05b13804fd1d0f7f9c843ae6b
sara-fs <sara.khosropour@firstsource.tech>
2022-01-18 11:39:32 +0330

    Fixed eslint issues of main

--- src/botPage/view/deriv/layout/Main/main.jsx
+++ src/botPage/view/deriv/layout/Main/main.jsx
@@ -1,38 +1,35 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import PropTypes from "prop-types";
-import Tour from "../../../tour";
-import {
-  get as getStorage,
-  isDone,
-} from "../../../../../common/utils/storageManager";
-import { updateShowTour } from "../../store/ui-slice";
-import Footer from "../Footer";
-import ToolBox from "../ToolBox";
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import Tour from '../../../tour';
+import { get as getStorage, isDone } from '../../../../../common/utils/storageManager';
+import { updateShowTour } from '../../store/ui-slice';
+import Footer from '../Footer';
+import ToolBox from '../ToolBox';
 import SidebarToggle from '../../components/SidebarToggle';
 
 
 const Main = ({api, blockly}) => {
-  const dispatch = useDispatch();
-  React.useEffect(() => {
-    const days_passed =
-      Date.now() >
-      (parseInt(getStorage("closedTourPopup")) || 0) + 24 * 60 * 60 * 1000;
-    dispatch(updateShowTour(isDone("welcomeFinished") || days_passed));
-  }, []);
-  return (
-    <div className="main">
-      <Tour />
-      <ToolBox blockly={blockly} />
-      <SidebarToggle />
-      <Footer api={api} />
-    </div>
-  );
+    const dispatch = useDispatch();
+    React.useEffect(() => {
+        const days_passed =
+        Date.now() >
+        (parseInt(getStorage('closedTourPopup')) || 0) + 24 * 60 * 60 * 1000;
+        dispatch(updateShowTour(isDone('welcomeFinished') || days_passed));
+    }, []);
+    return (
+        <div className="main">
+            <Tour />
+            <ToolBox blockly={blockly} />
+            <SidebarToggle />
+            <Footer api={api} />
+        </div>
+    );
 };
 
 Main.propTypes = {
     api: PropTypes.object.isRequired,
     blockly: PropTypes.object.isRequired,
-  }
+}
 export default Main;
 
