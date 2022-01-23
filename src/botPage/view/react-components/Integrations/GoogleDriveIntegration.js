3a1cbcb39f0e9833dd255bac223e933aa2db8e02
Salar Hafezi <salar@re-work.dev>
2022-01-23 16:05:06 +0330

    Fix: replace Google Drive ready status with login status

--- src/botPage/view/react-components/Integrations/GoogleDriveIntegration.js
+++ src/botPage/view/react-components/Integrations/GoogleDriveIntegration.js
@@ -4,14 +4,14 @@ import { translate } from '../../../../common/i18n';
 import google_drive_util from '../../../../common/integrations/GoogleDrive';
 
 const GoogleDriveIntegration = () => {
-    const { is_gd_ready } = useSelector(state => state.client);
+    const { is_gd_logged_in } = useSelector(state => state.client);
 
     return (
         <div className="integration input-row last">
             <div className="left">
                 <h2>Google Drive</h2>
                 <div className="description">{translate('Save your blocks and strategies to Google Drive')}</div>
-                {is_gd_ready && (
+                {is_gd_logged_in && (
                     <div className="integration-user">
                         {`${translate('You are logged in as')} ${google_drive_util.profile?.getEmail()}`}
                     </div>
@@ -20,11 +20,14 @@ const GoogleDriveIntegration = () => {
             <div className="right">
                 <a
                     onClick={() => google_drive_util.authorise()}
-                    className={!is_gd_ready ? 'button' : 'button-disabled'}
+                    className={!is_gd_logged_in ? 'button' : 'button-disabled'}
                 >
                     <span id="connect-google-drive">{translate('Connect')}</span>
                 </a>
-                <a onClick={() => google_drive_util.logout()} className={is_gd_ready ? 'button' : 'button-disabled'}>
+                <a
+                    onClick={() => google_drive_util.logout()}
+                    className={is_gd_logged_in ? 'button' : 'button-disabled'}
+                >
                     <span id="disconnect-google-drive">{translate('Disconnect')}</span>
                 </a>
             </div>
