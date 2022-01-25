e5e1739384cec7077640c5051c8bda90a5daf73f
Salar Hafezi <salar@re-work.dev>
2022-01-25 14:58:28 +0330

    Fix: Do not round while getting last digit

--- src/botPage/bot/tools.js
+++ src/botPage/bot/tools.js
@@ -47,7 +47,7 @@ export const getDirection = ticks => {
     return direction;
 };
 
-export const getLastDigit = (tick, pipSize) => Number(tick.toFixed(pipSize).slice(-1)[0]);
+export const getLastDigit = tick => (Number.isInteger(tick) ? tick % 10 : Number(tick.toString().slice(-1)));
 
 export const subscribeToStream = (observer, name, respHandler, request, registerOnce, type, unregister) =>
     new Promise(resolve => {
