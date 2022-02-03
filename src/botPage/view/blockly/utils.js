38b1dcfc41680fe2b879de986cf52d6478f59269
sandeep <sandeep@deriv.com>
2022-02-03 11:06:58 +0800

    made suggested changed and removed var

--- src/botPage/view/blockly/utils.js
+++ src/botPage/view/blockly/utils.js
@@ -173,9 +173,9 @@ export const deleteBlockIfExists = block => {
 export const setBlockTextColor = block => {
     Blockly.Events.recordUndo = false;
     if (block.inputList instanceof Array) {
-        Array.from(block.inputList).forEach(inp => {
-            if (inp?.fieldRow?.length) {
-                inp.fieldRow.forEach(field => {
+        Array.from(block.inputList).forEach(input => {
+            if (input?.fieldRow?.length) {
+                input.fieldRow.forEach(field => {
                     if (field instanceof Blockly.FieldLabel) {
                         const svgElement = field.getSvgRoot();
                         if (svgElement) {
