2e1a5b3a943e02b56ec4a29bd64a96a1916d56d6
sara-fs <sara.khosropour@firstsource.tech>
2022-02-13 18:24:59 +0330

    Applied changes after resolving conflicts

--- src/botPage/view/deriv/components/tour/tour-targets.jsx
+++ src/botPage/view/deriv/components/tour/tour-targets.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderHTML from 'react-render-html';
-import { isLoggedInDeriv } from '../../../../../common/utils/storageManager';
+import { useSelector } from "react-redux";
+import { useEffect } from 'react';
 
 const FirstStepTarget = () => ( <div
     style={{
@@ -32,10 +33,12 @@ const ThirdStepTarget = () => {
 };
 
 const ForthStepTarget = () => {
+  const { is_logged } = useSelector((state) => state.client);
+
     const style = 
-        `top:50px;left: ${isLoggedInDeriv() ? '84%': '90%'};position:absolute;width:1em;height:1em`;
-    return createReactNode('forth-step-target', style, isLoggedInDeriv() ? 
-        'acc_switcher' : 'btn__login', isLoggedInDeriv() ? 
+        `top:50px;left: ${is_logged ? '84%': '90%'};position:absolute;width:1em;height:1em`;
+    return createReactNode('forth-step-target', style, is_logged ? 
+        'acc_switcher' : 'btn__login', is_logged ? 
         'header__menu-right' : 'header__btn');
 };
 
@@ -49,13 +52,20 @@ function createReactNode(id, style, current_node, parent_node){
     return renderHTML(new_account_position.outerHTML);
 }
 
-const TourTargets = () => (
+const TourTargets = () => {
+    const { is_header_loaded } = useSelector(state=>state.ui);
+
+    useEffect(()=>{
+
+    },[is_header_loaded]);
+
+    return is_header_loaded && 
     <div>
         <FirstStepTarget />
         <SecontStepTarget />
         <ThirdStepTarget />
         <ForthStepTarget />
     </div>
-);
+};
 
 export default TourTargets;
