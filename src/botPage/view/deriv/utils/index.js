85fdc69f3c081d0f39b944a0c713477eb025a4eb
mehrzad-fs <mehrzad@firstsource.tech>
2022-02-23 14:54:10 +0330

    add .be to productions list

--- src/botPage/view/deriv/utils/index.js
+++ src/botPage/view/deriv/utils/index.js
@@ -1,16 +1,2 @@
 export * from "./account-methods";
-
-export const generateDerivLink = (path, ...queries) => {
-  let origin = "https://app.deriv.com";
-  const split_host_name = /^(staging-)?(bot.deriv.)([a-zA-Z]*)$/.exec(window.location.hostname);
-  if(split_host_name){
-    const prefix = split_host_name[1] || "";
-    const extension = split_host_name[3] || "";
-    if(["com","me","be"].includes(extension)){
-      origin =  `https://${prefix}app.deriv.${extension}/`;
-    }
-  }
-  const redirect_query = `ext_platform_url=${encodeURIComponent(window.location.origin)}`;
-  queries.push(redirect_query);
-  return `${origin + path}?${queries.join("&")}`;
-};
+export * from "./deriv-links";
