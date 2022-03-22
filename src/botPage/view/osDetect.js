fcfb8a9f5bc44dc0af74762cf1d2a0226d0e2921
Sandeep <sandeep@deriv.com>
2022-03-22 16:56:20 +0800

    Removed unused methods

--- src/botPage/view/osDetect.js
+++ src/botPage/view/osDetect.js
@@ -1,28 +1,5 @@
-export const MOBILE_TYPE = {
-    WINDOWS_PHONE: 'Windows Phone',
-    ANDROID      : 'Android',
-    IOS          : 'iOS',
-    UNKNOWN      : 'unknown',
-};
-
-export const mobileOSDetect = () => {
+/* eslint-disable import/prefer-default-export */
+export const isIOS = () => {
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-    // Windows Phone must come first because its UA also contains "Android"
-    if (/windows phone/i.test(userAgent)) {
-        return MOBILE_TYPE.WINDOWS_PHONE;
-    }
-
-    if (/android/i.test(userAgent)) {
-        return MOBILE_TYPE.ANDROID;
-    }
-
-    // iOS detection from: http://stackoverflow.com/a/9039885/177710
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        return MOBILE_TYPE.IOS;
-    }
-
-    return MOBILE_TYPE.UNKNOWN;
+    return !!(/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream);
 };
-
-export const isIOS = () => !!(mobileOSDetect() === MOBILE_TYPE.IOS);
