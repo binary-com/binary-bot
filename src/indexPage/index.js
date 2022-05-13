8eb5a60e657426b2493a9e45747e89a8ff664a50
mitra-deriv <mitra@regentmarkets.com>
2022-05-13 14:11:04 +0800

    initial setup

--- src/indexPage/index.js
+++ src/indexPage/index.js
@@ -8,44 +8,74 @@ import { isEuCountry, showHideEuElements } from '../common/footer-checks';
 import GTM from '../common/gtm';
 import { load as loadLang, showBanner } from '../common/lang';
 import { moveToDeriv } from '../common/utils/utility';
-import { getTokenList, set as setStorage, get as getStorage } from '../common/utils/storageManager';
+import { get as getStorage, set as setStorage, remove, getTokenList } from '../common/utils/storageManager';
 import { createUrl } from '../common/utils/tools';
 import '../common/binary-ui/dropdown';
+import BotLanding from './react-components/bot-landing';
 
-const movetoderiv = getStorage('movetoDeriv');
+const today = new Date().getTime();
+const expirationDate = today + 1000 * 60 * 60 * 24 * 21;
+
+const elements = ['#notification-banner', '#main', '#footer', '#header', '#topbar'];
+const temp = getStorage('movetoderiv');
+const renderBanner = () => {
+    ReactDOM.render(<BotLanding />, document.getElementById('movetoderiv'));
+    // mitra-deriv routing to be handled later
+    // setStorage('movetoderiv', expirationDate);
+    for (let i = 0; i < elements.length; i++) {
+        document.querySelector(elements[i]).classList.add('hidden');
+    }
+    document.getElementById('movetoderiv').classList.remove('hidden');
+    document.getElementById('bot-main').classList.remove('hidden');
+    $('.barspinner').hide();
+};
 
 const renderElements = () => {
-    ReactDOM.render(<Logo />, document.getElementById('binary-logo'));
-    ReactDOM.render(<Footer />, document.getElementById('footer'));
-    isEuCountry().then(isEu => showHideEuElements(isEu));
-    showBanner();
+    $('.barspinner').show();
 
-    $('#shop-url').attr(
-        'href',
-        createUrl({
-            subdomain   : 'shop',
-            path        : 'collections/strategies',
-            isNonBotPage: true,
-        })
-    );
+    if (!temp) {
+        renderBanner();
+    } else {
+        if (today > temp) {
+            remove('movetoderiv');
+            renderBanner();
+            return false;
+        }
+        ReactDOM.render(<Logo />, document.getElementById('binary-logo'));
+        ReactDOM.render(<Footer />, document.getElementById('footer'));
+        isEuCountry().then(isEu => showHideEuElements(isEu));
+        showBanner();
+        $('#shop-url').attr(
+            'href',
+            createUrl({ subdomain: 'shop', path: 'collections/strategies', isNonBotPage: true })
+        );
+        for (let i = 0; i < elements.length; i++) {
+            document.querySelector(elements[i]).classList.remove('hidden');
+        }
+        document.getElementById('movetoderiv').classList.add('hidden');
+        document.getElementById('bot-main').classList.remove('hidden');
+        $('.barspinner').hide();
+    }
 };
 
 const loginCheck = () => {
-    setStorage('movetoDeriv', 'move');
     if (endpoint()) return;
     moveToDeriv();
-    if (!movetoderiv) {
-        window.location.pathname = `${window.location.pathname.replace(/\/+$/, '')}/movetoderiv.html`;
-    } else if (getTokenList().length) {
-        window.location.pathname = `${window.location.pathname.replace(/\/+$/, '')}/bot.html`;
+    loadLang();
+    if (temp) {
+        if (getTokenList().length) {
+            window.location.pathname = `${window.location.pathname.replace(/\/+$/, '')}/bot.html`;
+            document.getElementById('bot-main').classList.remove('hidden');
+        } else {
+            oauthLogin(() => {
+                $('.show-on-load').show();
+                $('.barspinner').hide();
+                renderElements();
+                GTM.init();
+            });
+        }
     } else {
-        loadLang();
-        oauthLogin(() => {
-            $('.show-on-load').show();
-            $('.barspinner').hide();
-            renderElements();
-            GTM.init();
-        });
+        renderBanner();
     }
 };
 
