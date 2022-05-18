bbb42c753586eb632358e3e764a893b0bf73d0a0
“pavel-latyshou-binary” <pavel.latyshou@binary.com>
2022-05-18 16:11:59 +0300

    feat: implement carousel

--- src/indexPage/react-components/bot-landing/Carousel.jsx
+++ src/indexPage/react-components/bot-landing/Carousel.jsx
@@ -4,50 +4,69 @@ import { translate } from '../../../common/i18n';
 
 const carouselContentArray = [
     {
-        title      : translate('Where to find Binary Bot on Deriv'),
-        content    : translate('On the Deriv homepage, hit Trade at the top of the page and choose Binary Bot.'),
-        action_text: translate('Try Binary Bot on Deriv'),
-        img        : 'image/1a.webm',
+        url: 'https://smarttrader.deriv.com/',
+        action_text: translate('Check it out'),
+        img: 'image/landing-carousel/smart-trader.png',
     },
     {
-        title      : translate('It’s the same Binary Bot you know and love'),
-        content    : translate('Everything is exactly the way you like it.'),
-        action_text: translate('Try Binary Bot on Deriv'),
-        img        : 'image/2a.webm',
+        url: 'https://bot.deriv.com/',
+        action_text: translate('Check it out'),
+        img: 'image/landing-carousel/binary-bot.png',
     },
     {
-        title      : translate('Bring your bots across seamlessly'),
-        content    : translate('Your strategies from Binary.com can be used on Deriv. Just load your XML files and get trading.'),
-        action_text: translate('Try Binary Bot on Deriv'),
-        img        : 'image/3a.webm',
+        url: 'https://deriv.com/dmt5/',
+        action_text: translate('Learn more'),
+        img: 'image/landing-carousel/mt5.png',
+    },
+    {
+        url: 'https://deriv.com/dtrader/',
+        action_text: translate('Learn more'),
+        img: 'image/landing-carousel/dtrader.png',
+    },
+    {
+        url: 'https://deriv.com/dbot/',
+        action_text: translate('Learn more'),
+        img: 'image/landing-carousel/dbot.png',
+    },
+    {
+        url: 'https://deriv.com/landing/deriv-go/',
+        action_text: translate('Learn more'),
+        img: 'image/landing-carousel/deriv-go.png',
+    },
+    {
+        url: 'https://deriv.com/derivx/',
+        action_text: translate('Learn more'),
+        img: 'image/landing-carousel/derivX.png',
     },
 ]
+
 
 const Carousel = () => {
 
     const settings = {
-        dots          : false,
-        infinite      : true,
-        speed         : 1000,
-        slidesToShow  : 1,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
         slidesToScroll: 1,
-        arrows        : true,
-        autoplay      : true,
-        autoplaySpeed : 5000,
-        nextArrow     : <span className='arrow'><img src='./image/icon.svg' /></span>,
-        prevArrow     : <span className='arrow right'><img src='./image/icon.svg' /></span>,
-        responsive    : [
+        arrows: false,
+        initialSlide: 1,
+        // autoplay: true,
+        // autoplaySpeed: 5000,
+        // nextArrow: <span className='arrow'><img src='./image/icon.svg' /></span>,
+        // prevArrow: <span className='arrow right'><img src='./image/icon.svg' /></span>,
+        responsive: [
             {
                 breakpoint: 1024,
-                settings  : {
-                    slidesToShow  : 1,
+                settings: {
+                    slidesToShow: 1,
                     slidesToScroll: 1,
                 },
             },
             {
                 breakpoint: 700,
-                settings  : {
-                    slidesToShow  : 1,
+                settings: {
+                    slidesToShow: 1,
                     slidesToScroll: 1,
                 },
             },
@@ -55,8 +74,29 @@ const Carousel = () => {
     };
 
     return (
-        <div className="carousel-wrapper section-container">
+        <div className="">
+
             <Slider {...settings}>
+                {carouselContentArray.map((slide, index) => {
+                    const { url, action_text, img } = slide;
+                    return (
+                        <div className="slide_card">
+                            <img className="slide_img" src={img} />
+                            <a href={url} target="_blank">
+                                <button className="arrow-btn">
+                                    <div className="btn_inner">
+                                        {action_text}
+                                        <img src={`image/landing-carousel/arrow.svg`} />
+
+                                    </div>
+                                </button>
+                            </a>
+                        </div>
+                    )
+                }
+                )}
+            </Slider>
+            {/* <Slider {...settings}>
                 {carouselContentArray.map((slide, index) => {
                     const { title, content, action_text, img } = slide;
                     return (
@@ -71,13 +111,12 @@ const Carousel = () => {
                             <div className='landing_carousel_placeholder'>
                                 <div className="bot-gif-placeholder">
                                     <div className="bot-gif-placeholder__background">
-                                        <img src="image/laptop_carousel.webp" />
+                                        <img src={img} />
                                         <div className="bot-gif-placeholder__background__overlay">
                                             <video autoPlay muted loop>
                                                 <source src={img} type="video/mp4" />
                                                 <source src={img} type="video/ogg" />
                                             </video>
-
                                         </div>
                                     </div>
                                 </div>
@@ -86,7 +125,7 @@ const Carousel = () => {
                     )
                 }
                 )}
-            </Slider>
+            </Slider> */}
         </div>
     );
 }
