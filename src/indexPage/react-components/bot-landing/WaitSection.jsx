f1091711f8df212d78b583e3151dfed728e3f2f1
mitra-deriv <mitra@regentmarkets.com>
2022-05-24 16:07:53 +0800

    fix: update the content and make responsive

--- src/indexPage/react-components/bot-landing/WaitSection.jsx
+++ src/indexPage/react-components/bot-landing/WaitSection.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { translate } from '../../../common/i18n';
 
+
+
 const WaitSection = () => (
     <section className='wait-wrapper'>
         <div className='wait-wrapper-inner section-container'>
@@ -8,14 +10,18 @@ const WaitSection = () => (
                 <h1>{translate('Don’t wait')}</h1>
                 <h2>{translate('Future-proof your trading. Come over to Deriv now.')}</h2>
                 <div className="btn-group">
-                    <a href="https://bot.deriv.com" target="_blank">
-                        <button className="l-btn danger">{translate('Trade Binary Bot on Deriv')}</button>
+                    <a href="https://deriv.com" target="_blank">
+                        <button className="l-btn danger">{translate('Enter the Deriv experience')}</button>
                     </a>
                 </div>
             </div>
+           
+            <img src="../image/dont-wait-image.png"  className='wait-wrapper-inner-placeholder-desktop'/>
             <div className='wait-wrapper-inner-placeholder'>
-                <img src="image/dont-wait-image.png "></img>
+            <img src="../image/dtrader-mobile.png"  className='wait-wrapper-inner-placeholder-mobile'/>
+                <h1>{translate ('Come over to Deriv and future-proof your trading now.')}</h1>
             </div>
+          
         </div>
 
     </section>
