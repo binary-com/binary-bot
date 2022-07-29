75c5187cafffc1ab7b0b2ee470fb32d1e4d35ca2
mitra-deriv <mitra@regentmarkets.com>
2022-07-29 09:53:52 +0800

    fix translation strings

--- src/indexPage/react-components/binary-landing/Card.jsx
+++ src/indexPage/react-components/binary-landing/Card.jsx
@@ -4,23 +4,23 @@ import { translate } from '../../../common/i18n';
 const CardSection = () => {
     const cardContentArray = [
         {
-            title  : translate('Trade multipliers'),
-            content: translate('Amplify your potential profit without risking more than your stake.'),
+            title  : 'Trade multipliers',
+            content: 'Amplify your potential profit without risking more than your stake.',
             img    : 'image/multiplier.png',
         },
         {
-            title  : translate('Peer-to-peer payments'),
-            content: translate('Exchange your local currency with fellow traders to get funds in and out of your account with Deriv P2P.'),
+            title  : 'Peer-to-peer payments',
+            content: 'Exchange your local currency with fellow traders to get funds in and out of your account with Deriv P2P.',
             img    : 'image/p2p.png',
         },
         {
-            title  : translate('Learn with Deriv Academy'),
-            content: translate('Enjoy complimentary articles and videos to help you learn the ropes of online trading.'),
+            title  : 'Learn with Deriv Academy',
+            content: 'Enjoy complimentary articles and videos to help you learn the ropes of online trading.',
             img    : 'image/academy.png',
         },
         {
-            title  : translate('Crash/Boom indices'),
-            content: translate('Predict and potentially gain from exciting spikes and dips. Only available with multipliers on DTrader.'),
+            title  : 'Crash/Boom indices',
+            content: 'Predict and potentially gain from exciting spikes and dips. Only available with multipliers on DTrader.',
             img    : 'image/crashboom.png',
         },
     ]
@@ -29,9 +29,9 @@ const CardSection = () => {
         <div className='card-container card-wrapper'>
             <div className='section-container-holder card-wrapper-holder'>
                 <div className="container-header  card-wrapper-holder-header">
-                    <h1>{translate ('Exclusively on Deriv')}</h1>
+                    <h1>{translate('Exclusively on Deriv')}</h1>
                     <h2>{translate('There’s a bright future ahead.')}</h2>
-                    <h2>{translate ('Find these bonus features and more on Deriv:')}</h2>
+                    <h2>{translate('Find these bonus features and more on Deriv:')}</h2>
                 </div>
             </div>
             <div className="card-container card-wrapper-holder-container">
@@ -42,8 +42,8 @@ const CardSection = () => {
                             <div>
                                 <img src={img} />
                             </div>
-                            <h1>{title}</h1>
-                            <h2>{content}</h2>
+                            <h1>{translate(title)}</h1>
+                            <h2>{translate(content)}</h2>
                         </div>
                     )
                 })
