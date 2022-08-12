e816e9c61f1ec82cf3e2246af4214675474c4d34
mitra-deriv <mitra@regentmarkets.com>
2022-08-12 17:06:10 +0800

    fix: remove the absolute path for the images

--- src/indexPage/react-components/binary-landing/TakeDeriv.jsx
+++ src/indexPage/react-components/binary-landing/TakeDeriv.jsx
@@ -5,13 +5,13 @@ const TakeDeriv = () => (
     <div className="take-deriv">
         <div className="take-deriv_background">
             <div className="take-deriv_iphone">
-                <img src="../image/phone-desctop.png" />
+                <img src="/image/phone-desctop.png" />
             </div>
             <div className="take-deriv_qr-code">
                 <h1>{translate('Take Deriv with you')}</h1>
                 <h2>{translate('Get the Deriv GO mobile app and trade on the go. Never miss golden opportunities again.')}</h2>
                 <div>
-                    <img src="../image/qr-code.png" />
+                    <img src="/image/qr-code.png" />
                 </div>
                 <h3>{translate('Scan this QR code to download Deriv GO')}</h3>
             </div>
