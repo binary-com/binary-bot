0b44464698984ccc8cb5cc2c262a97d5751b48a5
mitra-deriv <mitra@regentmarkets.com>
2023-03-29 12:12:59 +0800

    feat: :sparkles: change exipry of cookie to 1 hour

--- src/indexPage/react-components/binary-landing/utils.js
+++ src/indexPage/react-components/binary-landing/utils.js
@@ -2,6 +2,6 @@ import Cookies from 'js-cookie';
 
 // eslint-disable-next-line import/prefer-default-export
 export const setBinaryCookieAndRedirect = url => {
-    Cookies.set('row-lp-visited', true, { expires: 1 });
+    Cookies.set('row-lp-visited', true, { expires: 1 / 24 });
     window.open(url, '_self');
 };
