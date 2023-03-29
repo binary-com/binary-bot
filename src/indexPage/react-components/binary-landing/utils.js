10c4a02a5b957d304385bbb98d9e3de17f1e2cdb
mitra-deriv <mitra@regentmarkets.com>
2023-03-29 11:47:08 +0800

    fix: :necktie: update cookie expiry day from 7 to 1

--- src/indexPage/react-components/binary-landing/utils.js
+++ src/indexPage/react-components/binary-landing/utils.js
@@ -2,6 +2,6 @@ import Cookies from 'js-cookie';
 
 // eslint-disable-next-line import/prefer-default-export
 export const setBinaryCookieAndRedirect = url => {
-    Cookies.set('row-lp-visited', true, { expires: 7 });
+    Cookies.set('row-lp-visited', true, { expires: 1 });
     window.open(url, '_self');
 };
